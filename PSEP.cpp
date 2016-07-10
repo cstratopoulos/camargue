4790867b00d810b4678aba250561cc21e1d99b90
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-07-10 15:16:10 -0400

    branch renaming

--- PSEP.cpp
+++ PSEP.cpp
@@ -21,7 +21,7 @@ int main(int argc, char* argv[]){
   vector<int> tour_node_indices;
   CCdatagroup dat;
 
-  cout << "BRANCH VERSION: MASTER" << endl;
+  cout << "BRANCH VERSION: PIVOT BACK w COPY BASE" << endl;
 
   if(load_tsplib(graph, &dat, argc, argv)){
     cerr << "Problem getting tsplib" << endl;
