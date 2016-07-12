9045238a2944ab0e8244db075d6f83e3258083e2
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-07-12 02:37:54 -0400

    rename

--- PSEP.cpp
+++ PSEP.cpp
@@ -22,7 +22,7 @@ int main(int argc, char* argv[]){
   Graph graph;
   vector<int> tour_node_indices;
 
-  cout << "BRANCH VERSION: LP WRAPPER" << endl;
+  cout << "BRANCH VERSION: LP WRAPPER/ENUM PIVOT LABELS??" << endl;
 
   if(initial_parse(argc, argv, graph, tour_node_indices)){
     cerr << "Problem parsing arguments" << endl;
