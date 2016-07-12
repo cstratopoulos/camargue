5bd5550ae8d1fffa46006a1bf490f6bfd2426db5
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-07-12 14:26:26 -0400

    relabel

--- PSEP.cpp
+++ PSEP.cpp
@@ -22,7 +22,7 @@ int main(int argc, char* argv[]){
   Graph graph;
   vector<int> tour_node_indices;
 
-  cout << "BRANCH VERSION: LP WRAPPER/ENUM PIVOT LABELS??" << endl;
+  cout << "BRANCH VERSION: LP WRAPPER/ENUM PIVOT/TEETH" << endl;
 
   if(initial_parse(argc, argv, graph, tour_node_indices)){
     cerr << "Problem parsing arguments" << endl;
