4c19141db3c342f76108bafe2d856011f6b10462
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-07-26 14:44:11 -0400

    bbnode

--- BButils.h
+++ BButils.h
@@ -2,7 +2,37 @@
 #define PSEP_BBUTILS_H
 
 class PSEP_BBNode {
+ public:
+ PSEP_BBNode(PSEP_BBNode::NType _type, int _edge) :
+  node_type(_type), branch_edge(_edge), node_stat(PSEP_BBNode::UNVISITED) {}
   
+  enum class NType {
+    ROOT, LEFT, RIGHT
+  };
+
+  NType &type() const {
+    return node_type;
+  }
+
+  enum class NStat {
+    UNVISITED, VISITED, FATHOMED
+  };
+
+  NStat &status() const {
+    return node_stat;
+  }
+
+  int edge() const {
+    if(node_type != NType::ROOT)
+      return branch_edge;
+    else
+      return -1;
+  }
+
+ private:
+  PSEP_BBNode::Ntype node_type;
+  PSEP_BBNode::NStat node_stat;
+  int branch_edge;
 };
 
 #endif
