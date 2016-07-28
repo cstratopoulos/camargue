d28ce5b7587cb5c507ad579a0e9bada38d71d0a8
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-07-28 14:53:36 -0400

    removed some timer statements

--- LPfixing.cpp
+++ LPfixing.cpp
@@ -1,32 +1,34 @@
 #include<iostream>
+#include<algorithm>
 #include<math.h>
 
 #include "LPfixing.h"
 
 using namespace std;
 
-int PSEP_LPfix::price(){
+int PSEP_LPfix::price(int *clamptotal, int *deltotal){
   int rval = 0;
-  int fixtotal = 0;
   int ecount = m_graph.edge_count;
   vector<double> redcosts(ecount);
   double GAP, lp_LB, cur_red, cur_abs;
-  double start;
+  double opt_time;
+  double total_time = PSEP_zeit();
+  *clamptotal = 0; *deltotal = 0;
 
   int infeasible = 0;
-  start = PSEP_zeit();
+  opt_time = PSEP_zeit();
   rval = PSEPlp_primal_opt(&m_lp, &infeasible);
   if(rval) goto CLEANUP;
-  start = PSEP_zeit() - start;
+  opt_time = PSEP_zeit() - opt_time;
   cout << "Optimized LP relaxation in "
-       << start << "s, objective val: ";
+       << opt_time << "s, objective val: ";
 
   rval = PSEPlp_objval(&m_lp, &lp_LB);
   if(rval) goto CLEANUP;
   cout << lp_LB << "\n";
 
   GAP = m_min_tour_value - lp_LB;
-  cout << "Integrality gap: " << GAP << "\n";
+  cout << "Integrality gap: " << GAP << ", ";
 
   rval = PSEPlp_get_redcosts(&m_lp, &redcosts[0]);
   if(rval) goto CLEANUP;
@@ -39,22 +41,108 @@ int PSEP_LPfix::price(){
       continue;
     }
 
-    fixtotal++;
     if(cur_red > 0){
       edge_delset[i] = FixStats::DELETE;
+      (*deltotal)++;
       continue;
     }
 
     if(cur_red < 0){
       edge_delset[i] = FixStats::FIXED;
+      (*clamptotal)++;
     }
   }
 
-  cout << fixtotal << " total edges can be fixed\n";
+  cout << *clamptotal << " edges shall be fixed to 1, "
+       << *deltotal << " shall be deleted.\n"
+       << (*clamptotal + *deltotal) << " total fixable, computed in "
+       << (PSEP_zeit() - total_time) << "s\n";
   
 
  CLEANUP:
   if(rval)
     cerr << "Error entry point: LPfixing::price() \n";
+  return rval;
+}
+
+int PSEP_LPfix::fixup(){
+  int rval = 0;
+  int total_time = PSEP_zeit();
+
+  for(int i = 0; i < edge_delset.size(); i++){
+    if(edge_delset[i] == FixStats::FIXED){
+      rval = PSEPlp_clampbnd(&m_lp, i, 'L', 1.0);
+      if(rval) goto CLEANUP;
+      edge_delset[i] = FixStats::LEAVE;
+    }
+  }
+
+  total_time += PSEP_zeit() - total_time;
+  
+ CLEANUP:
+  if(rval)
+    cerr << "Error entry point: LPfix::fixup \n";
+  return rval;
+}
+
+int PSEP_LPfix::delete_cols(){
+  int rval = PSEPlp_delsetcols(&m_lp, &edge_delset[0]);
+  if(rval)
+    cerr << "Error entry point: LPfix::delete_cols \n";
+  return rval;
+}
+
+void PSEP_LPfix::delete_edges(){
+  double total_time = PSEP_zeit();
+  for(int i = 0; i < m_graph.edge_count; i++)
+    edge_lookup[IntPair(edges[i].end[0], edges[i].end[1])] = edge_delset[i];
+
+  vector<Edge> new_graph_edges;
+  vector<int> new_best_edges;
+
+  for(int i = 0; i < edge_delset.size(); i++){
+    if(edge_delset[i] != -1){
+      new_best_edges.push_back(best_tour_edges[i]);
+      new_graph_edges.emplace_back(edges[i]);
+    }
+  }
+
+  edges.clear();
+  edges = new_graph_edges;
+  best_tour_edges.clear();
+  best_tour_edges = new_best_edges;
+  
+  m_graph.edge_count = edges.size();
+  delta.resize(m_graph.edge_count);
+  m_lp_edges.resize(m_graph.edge_count);
+
+  total_time = PSEP_zeit() - total_time;
+
+  cout << "Number of edges is now " << edges.size() << ", deleted in "
+       << total_time << "s\n";
+}
+
+
+int PSEP_LPfix::redcost_fixing(){
+  int rval = 0;
+  int clamptotal = 0, deltotal = 0;
+
+  rval = price(&clamptotal, &deltotal);
+  if(rval) goto CLEANUP;
+
+  if(clamptotal > 0){
+    rval = fixup();
+    if(rval) goto CLEANUP;
+  }
+
+  if(deltotal > 0){
+    rval = delete_cols();
+    if(rval) goto CLEANUP;
+    delete_edges();
+  }
+
+ CLEANUP:
+  if(rval)
+    cerr << "Problem in LPfix::redcost_fixing.\n";
   return rval;
 }
--- LPfixing.h
+++ LPfixing.h
@@ -13,6 +13,7 @@ class PSEP_LPfix {
  PSEP_LPfix(PSEP_BestGroup &BestGroup, PSEP_GraphGroup &GraphGroup,
 	    PSEP_LPGroup &LPGroup) :
   m_min_tour_value(BestGroup.min_tour_value),
+    best_tour_edges(BestGroup.best_tour_edges),
     m_graph(GraphGroup.m_graph), edges(GraphGroup.m_graph.edges),
     edge_lookup(GraphGroup.m_graph.edge_lookup), delta(GraphGroup.delta),
     m_lp(LPGroup.m_lp), m_lp_edges(LPGroup.m_lp_edges)
@@ -22,16 +23,17 @@ class PSEP_LPfix {
     LEAVE = 0, DELETE = 1, FIXED = 2
   };
 
-  int price();
+  int price(int *clamptotal, int *deltotal);
   int fixup();
   int delete_cols();
-  int delete_edges();
+  void delete_edges();
 
   int redcost_fixing();
   
 
  private:
   double &m_min_tour_value;
+  std::vector<int> &best_tour_edges;
   
   Graph &m_graph;
   std::vector<Edge> &edges;
@@ -41,7 +43,8 @@ class PSEP_LPfix {
   PSEPlp &m_lp;
   std::vector<double> &m_lp_edges;
 
-  std::vector<int> edge_delset;    
+  std::vector<int> edge_delset;
+
 };
 
 #endif
