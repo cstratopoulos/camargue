6ae0fb2a1e58225337e9ba7282af71a02fca326d
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-07-30 13:51:27 -0400

    new versions

---
+++ blossoms.cpp
@@ -0,0 +1,142 @@
+#include<iostream>
+
+#include "blossoms.h"
+
+using namespace std;
+using namespace PSEP;
+
+int Cut<blossom>::separate(){
+  int rval = 0;
+  int cut_edge_index;
+  int end0, end1;
+  int best_tour_entry;
+  int ncount = 2 * support_indices.size();
+  double orig_weight, changed_weight, cutval;
+  int *cut_nodes = (int *) NULL;
+  int cutcount = 0;
+
+  cut_ecap.resize(support_ecap.size());
+
+  for(int i = 0; i < support_indices.size(); i++){
+    if(best_tour_edges[support_indices[i]] == 0)
+      cut_ecap[i] = support_ecap[i];
+    else
+      cut_ecap[i] = 1 - support_ecap[i];
+  }
+
+  for(int i = 0; i < support_indices.size(); i++){
+    cut_edge_index = support_indices[i];
+    best_tour_entry = best_tour_edges[cut_edge_index];
+
+    switch(best_tour_entry){
+    case(0):
+      orig_weight = support_ecap[i];
+      changed_weight = 1 - support_ecap[i];
+      break;
+    case(1):
+      orig_weight = 1 - support_ecap[i];
+      changed_weight = support_ecap[i];
+    }
+
+    cut_ecap[i] = changed_weight;
+
+    end0 = support_elist[2 * i];
+    end1 = support_elist[(2 * i) + 1];
+
+    rval = CCcut_mincut_st(ncount, support_indices.size(),
+			   &support_elist[0], &cut_ecap[0], end0, end1,
+			   &cutval, &cut_nodes, &cutcount);
+    if(rval){
+      cerr << "Problem in blossom::separate w st-cut" << endl;
+      goto CLEANUP;
+    }
+
+    if(cutval < 1 - LP::EPSILON){
+      if(!best || cutval < best->cut_val){
+	vector<int> handle;
+	for(int j = 0; j < cutcount; j++){
+	  handle.push_back(cut_nodes[j]);
+	}
+
+	best.reset(new blossom(handle, cut_edge_index, cutval));
+      }
+    }
+
+    cut_ecap[i] = orig_weight;
+  }
+
+  if(!best) rval = 2;
+
+ CLEANUP:
+  if(cut_nodes) free(cut_nodes);
+  return rval;
+}
+
+int Cut<blossom>::parse_coeffs(){
+  if(!best){
+    cerr << "Cuts<blossom>::parse_coeffs tried to parse null pointer\n";
+    return 1;
+  }
+
+  deltacount = 0;
+
+  G_Utils::get_delta(best->handle, edges, &deltacount, delta, edge_marks);
+
+  return 0;
+}
+
+int Cut<blossom>::add_cut(){
+  int rval = 0, newrows = 1, newnz = deltacount;
+  int rmatbeg[1] = {0};
+  char sense[1] = {'G'};
+  double rhs[1];
+  int num_teeth = 0;
+  vector<double> rmatval(deltacount, 1.0);
+  int cutedge = best->cut_edge;
+
+  switch(best_tour_edges[cutedge]){
+  case 0:
+    for(int i = 0; i < deltacount; i++)
+      if(m_lp_edges[delta[i]] > LP::EPSILON &&
+	 (best_tour_edges[delta[i]] == 1 || delta[i] == cutedge)){
+	rmatval[i] = -1.0;
+	num_teeth++;
+      }
+    break;
+  case 1:
+    for(int i = 0; i < deltacount; i++)
+      if(m_lp_edges[delta[i]] > LP::EPSILON &&
+	 (best_tour_edges[delta[i]] == 1 && delta[i] != cutedge)){
+	rmatval[i] = -1.0;
+	num_teeth++;
+      }
+  }
+
+  rhs[0] = 1 - num_teeth;  
+
+  rval = PSEPlp_addrows (&m_lp, newrows, newnz, rhs, sense, rmatbeg,
+			 &delta[0], &rmatval[0]);
+
+  if(rval)
+    cerr << "Entry point: Cut<blossom>::add_cut" << endl;
+  return rval;
+}
+
+int Cut<blossom>::cutcall(){
+  int rval = 0;
+
+  rval = separate();
+  if(rval) goto CLEANUP;
+
+  rval = parse_coeffs();
+  if(rval || deltacount == 0) goto CLEANUP;
+
+  rval = add_cut();
+  if(rval) goto CLEANUP;
+
+ CLEANUP:
+  if(rval == 1)
+    cerr << "Problem in Cuts<blossom>::cutcall\n";
+  best.reset(NULL);
+  return rval;
+}
---
+++ segments.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+
+#include "segments.h"
+
+using namespace std;
+using namespace PSEP;
+
+int Cut<seg>::separate(){
+  int ncount = G_s.node_count;
+  int current_start, current_end, current_size;
+  SNode current_snode;
+  double lhs;
+
+  for(int i = 0; i < ncount - 2; i++){
+    current_start = best_tour_nodes[i];
+    edge_marks[current_start] = 1;
+    current_size = 1;
+    lhs = 0;
+    int j;
+    
+    for(j = i + 1; (j < ncount - 1 && (++current_size) <= ncount / 2); j++){
+      current_end = best_tour_nodes[j];
+      edge_marks[current_end] = 1;
+      current_snode = G_s.nodelist[current_end];
+      for(int k = 0; k < current_snode.s_degree; k++)
+	if(edge_marks[current_snode.adj_objs[k].other_end] == 1)
+	  lhs += current_snode.adj_objs[k].lp_weight;
+      
+      if(lhs > current_size - 1 && (fabs(lhs - (current_size - 1)) >= 0.002)){
+	if(!best || (lhs - (current_size - 1)) > best->viol)
+	  best.reset(new seg(i, j,lhs - (current_size - 1)));
+      }
+    }
+    for(int l = i; l <= j; l++)
+      edge_marks[best_tour_nodes[l]] = 0;
+  }
+
+  if(!best)
+    return 2;
+  
+  return 0;
+}
+
+int Cut<seg>::parse_coeffs(){
+  if(!best){
+    cerr << "Cuts<seg>::parse_coeffs tried to parse null pointer\n";
+    return 1;
+  }
+
+  deltacount = 0;
+
+  std::vector<int> segnodes;
+
+  for(int i = best->start; i <= best->end; i++)
+    segnodes.push_back(best_tour_nodes[i]);
+
+  G_Utils::get_delta(segnodes, edges, &deltacount, delta, edge_marks);
+  
+  return 0;
+}
+
+int Cut<seg>::add_cut(){
+  int rval = 0, newrows = 1, newnz = deltacount;
+  int rmatbeg[1] = {0};
+  char sense[1] = {'G'};
+  double rhs[1] = {2.0};
+  vector<double> rmatval(deltacount, 1.0);
+
+  rval = PSEPlp_addrows (&m_lp, newrows, newnz, rhs, sense, rmatbeg,
+			 &delta[0], &rmatval[0]);
+
+  if(rval)
+    cerr << "Entry point: PSEP_Segments::add_cut" << endl;
+  return rval;
+}
+
+int Cut<seg>::cutcall(){
+  int rval = 0;
+
+  rval = separate();
+  if(rval) goto CLEANUP;
+
+  rval = parse_coeffs();
+  if(rval || deltacount == 0) goto CLEANUP;
+
+  rval = add_cut();
+  if(rval) goto CLEANUP;
+
+ CLEANUP:
+  if(rval == 1)
+    cerr << "Problem in Cuts<seg>::cutcall()\n";
+  best.reset(NULL);
+  return rval;
+}
