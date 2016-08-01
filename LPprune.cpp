3d934e424627316bcd01cb8a3c03871d00626fbd
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-08-01 11:36:18 -0400

    moved cut pruning to separate modules

---
+++ LPprune.cpp
@@ -0,0 +1,60 @@
+#include "LPprune.h"
+
+using namespace std;
+using namespace PSEP;
+
+int LPPrune::prune_cuts(int &num_removed){
+  int rval = 0;
+  num_removed = 0;
+  int rowcount = PSEPlp_numrows(&m_lp);
+  vector<int> delset(rowcount, 0);
+  vector<double> slacks(rowcount - node_count, 0);
+
+  rval = PSEPlp_getslack(&m_lp, &slacks[0], node_count, rowcount - 1);
+  if(rval)
+    return 1;
+
+  for(int i = 0; i < slacks.size(); i++){
+    if(fabs(slacks[i]) >= LP::EPSILON){
+      delset[node_count + i] = 1;
+      num_removed++;
+    }
+  }
+
+  rval = PSEPlp_delsetrows(&m_lp, &delset[0]);
+  if(rval)
+    return 1;
+
+  return 0;
+}
+
+int LPPrune::prune_with_skip(int &num_removed, IntPair skiprange,
+			     vector<int> &delset){
+  int rval = 0;
+  num_removed = 0;
+  int rowcount = PSEPlp_numrows(&m_lp);
+  delset.clear();
+  delset.resize(rowcount, 0);
+  int skip_start = skiprange.first;
+  int skip_end = skiprange.second;
+  vector<double> slacks(rowcount - node_count, 0);
+
+  rval = PSEPlp_getslack(&m_lp, &slacks[0], node_count, rowcount - 1);
+  if(rval)
+    return 1;
+
+  for(int i = 0; i < slacks.size(); i++){
+    if(skip_start <= node_count + i && node_count + i <= skip_end)
+      continue;
+    if(fabs(slacks[i]) >= LP::EPSILON){
+      delset[node_count + i] = 1;
+      num_removed++;
+    }
+  }
+
+  rval = PSEPlp_delsetrows(&m_lp, &delset[0]);
+  if(rval)
+    return 1;
+
+  return 0;
+}
---
+++ LPprune.h
@@ -0,0 +1,25 @@
+#ifndef PSEP_LP_PRUNE_H
+#define PSEP_LP_PRUNE_H
+
+#include<vector>
+
+#include "lp.h"
+#include "datagroups.h"
+#include "PSEP_util.h"
+
+namespace PSEP {
+  class LPPrune {
+  public:
+  LPPrune(PSEP_GraphGroup &GraphGroup, PSEP_LPGroup &LPGroup) :
+    m_lp(LPGroup.m_lp), node_count(GraphGroup.m_graph.node_count) {}
+
+    int prune_cuts(int &num_removed);
+    int prune_with_skip(int &num_removed, IntPair skiprange,
+			std::vector<int> &delset);
+  private:
+    PSEPlp &m_lp;
+    int node_count;    
+  };
+}
+
+#endif
