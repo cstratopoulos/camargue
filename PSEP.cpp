395defb701e8b244e652ec4dcc80932247cb4047
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-08-10 15:09:13 -0400

    relabel

--- PSEP.cpp
+++ PSEP.cpp
@@ -21,7 +21,7 @@ int main(int argc, char* argv[]){
   CCdatagroup *dat = new CCdatagroup;
   char *fname;
 
-  cout << "BRANCH VERSION: MASTER (MIP CALLBACKS)\n";
+  cout << "BRANCH VERSION: NEW PIVOT\n";
 
   if(initial_parse(argc, argv, &fname, prefs)){
     cerr << "Problem parsing arguments" << endl;
