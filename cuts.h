30bc9c28694d9818865f54e7f589116d5e8fb1bf
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-08-19 14:07:22 -0400

    adding documentation

--- cuts.h
+++ cuts.h
@@ -1,3 +1,46 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+ *                                                                            
+ *                CUT TEMPLATE AND CUT STRUCTURE DEFINITIONS                  
+ *                                                                            
+ * An empty class template for the Cut data type used by the CutControl class 
+ * and structure definitions for each type of cut used therein.
+ * A specialization for a given cut type should support the named operations, 
+ * while including additional functions/data as needed. 
+ *                                                                            
+ *    CLASS TEMPLATES:                                                        
+ *      Cut<cut_type>
+ *        public:
+ *          int cut_call()
+ *          Wrapper function calling private member functions in order
+ *        private:
+ *          int separate(), parse_coeffs(), add_cut()
+ *          separate - the cut-specific (primal) separation routine which
+ *                     stores a cut, if found, in the cut-specific storage
+ *                     object. See specific headers for details. 
+ *                   - returns 0 if successful,
+ *                             1 if an error occurs,
+ *                             2 if no cut is found
+ *          parse_coeffs() - turns cut structure into coefficients that can be
+ *                           added to the LP.
+ *                         - returns 0 if successful, 1 if an error occurs
+ *          add_cut()    - adds the cut found to the LP
+ *                       - returns 0 if successful, 1 if an error occurs
+ *        
+ *    STRUCTURES:
+ *      seg
+ *        int start, int end, double viol
+ *      Stores segment cuts -- subtour cuts arising from segments of the 
+ *      incumbent best tour
+ *      start, end - the start and endpoints of the segment, indicated as 
+ *                   indices for accessing the vector best_tour_nodes. 
+ *                   Thus the associated segment is best_tour_nodes[start] to
+ *                   best_tour_nodes[end]
+ *      viol - the amount by which the cut is violated
+ *
+ *      
+
+\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #ifndef PSEP_CUTS_H
 #define PSEP_CUTS_H
 
@@ -45,10 +88,7 @@ namespace PSEP {
 
   struct general {
   general(bool _gomory, bool _disj, bool _mir) :
-    added_range(IntPair(0,0)), gomory_frac(_gomory),
-      disjunctive(_disj), rounding(_mir) {}
-    
-    IntPair added_range;
+    gomory_frac(_gomory), disjunctive(_disj), rounding(_mir) {}
 
     bool gomory_frac;
     bool disjunctive;
@@ -59,14 +99,12 @@ namespace PSEP {
   template<typename cut_t>
     class Cut {
   public:
-    int cut_call(){ return 1;}
+    //int cut_call(){ return 1;}
 
   private:
-    int separate(){ return 1;}
-    int parse_coeffs(){ return 1;}
-    int add_cut(){return 1;}
-
-    std::unique_ptr<cut_t> best;
+    /* int separate(){ return 1;} */
+    /* int parse_coeffs(){ return 1;} */
+    /* int add_cut(){return 1;} */
   };
 }
 
