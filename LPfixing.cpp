eee8e2196bfb2dfa5b08ff2b240b8f2865610105
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-09-16 13:44:50 -0400

    removed prints

--- LPfixing.cpp
+++ LPfixing.cpp
@@ -17,11 +17,6 @@ int EdgeFix::price(int *clamptotal, int *deltotal){
   int infeasible = 0;
 
   *clamptotal = 0; *deltotal = 0;
-
-  //
-  cout << "In EdgeFix::price....printing best tour\n";
-  PSEP::print_vec_nonzero(best_tour_edges);
-  //
   
   try {
     redcosts.resize(ecount);
@@ -56,9 +51,6 @@ int EdgeFix::price(int *clamptotal, int *deltotal){
 
     if(cur_red > 0 && best_tour_edges[i] != 1){
       edge_delset[i] = FixStats::DELETE;
-      //
-      cout << "Deleting edge " << i << "\n";
-      //
       (*deltotal)++;
       continue;
     }
