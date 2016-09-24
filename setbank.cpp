307ef4572370b45d63fc5d36f41d725d59e9741d
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-09-23 23:17:54 -0400

    new files for storing hypergraphs

---
+++ setbank.cpp
@@ -0,0 +1,124 @@
+#include "setbank.hpp"
+
+#include<algorithm>
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+namespace PSEP{
+
+std::size_t hash_value(const IntervalSet &S){
+  boost::hash<std::vector<IntPair>> hahser;
+  return hahser(S.interval_list);
+}
+  
+void hashing_test(){
+  IntervalSet set1({IntPair(1, 2), IntPair(3,4)});
+  IntervalSet set2({IntPair(3,5), IntPair(7,9)});
+  IntervalSet dup1({IntPair(3,4), IntPair(1,2)});
+  dup1.use_count++;
+
+  std::cout << "Created set 1:\n"; set1.print();
+  std::cout << "set1 use count: " << set1.use_count << ", dup1: "
+	    << dup1.use_count << "\n";
+  std::cout << "Test set1 == dup1: " << (set1 == dup1) << "\n";
+
+  SetHash set_bank;
+  set_bank.insert(set1);
+  set_bank.insert(set2);
+  std::cout << "After adding set1 and 2, set_bank size: "
+	    << set_bank.size() << "\n";
+  std::cout << "Trying to find dup1: " << set_bank.count(dup1) << "\n";
+
+  sort(dup1.interval_list.begin(), dup1.interval_list.end());
+
+  std::cout << "After sorting, dup1 == set1: " << (set1 == dup1) << "\n";
+  std::cout << "Trying to find dup1: " << set_bank.count(dup1) << "\n";
+}
+
+void interactive_test(){
+  int node_count;
+  cout << "Choose problem size: ";
+  cin >> node_count;
+
+  vector<int> tour_nodes(node_count);
+  vector<int> perm(node_count);
+
+  for(int i = 0; i < node_count; i++)
+    tour_nodes[i] = i;
+
+  random_shuffle(tour_nodes.begin(), tour_nodes.end());
+
+  for(int i = 0; i < tour_nodes.size(); i++){
+    perm[tour_nodes[i]] = i;
+  }
+
+  SetHash set_bank;
+  cout << "Declared empty set_bank\n";
+
+  char answer = 'y';
+
+  while(answer == 'y'){
+    vector<int> set_nodes;
+    int set_size, set_elem;
+    cout << "Enter size of nodeset to test: ";
+    cin >> set_size;
+    cout << "Enter nodes in set: \n";
+    for(int i = 0; i < set_size; i++){
+      cin >> set_elem;
+      set_nodes.push_back(set_elem);
+    }
+
+    IntervalSet newset(set_nodes, tour_nodes, perm);
+
+    set_bank.insert(newset);
+    cout << "Size of set bank is now: " << set_bank.size() << "\n";
+
+    cout << "Continue? y for yes: ";
+    cin >> answer;
+  }
+  cout << "Done testing.\n";  
+}
+
+}
+
+using namespace PSEP;
+
+IntervalSet::IntervalSet(vector<int> &nodelist,
+			 const vector<int> &best_tour_nodes,
+			 const vector<int> &perm) : use_count(0) {
+  int rval = 0;
+
+  sort(nodelist.begin(), nodelist.end(),
+       [perm](int &node1, int &node2) -> bool {
+	 return perm[node1] < perm[node2];
+       });
+
+
+  for(int i = 0; i < nodelist.size(); i++){
+    int start_ind = perm[nodelist[i]];
+    int end_ind = start_ind, cur_ind = start_ind;
+    
+    for(int j = i + 1; j < nodelist.size(); j++){
+      cur_ind++;
+      if(best_tour_nodes[cur_ind] == nodelist[j]){
+	end_ind = cur_ind;
+	i++;
+      } else {
+	break;
+      }
+    }
+
+    try { interval_list.push_back(IntPair(start_ind, end_ind)); }
+    catch(const bad_alloc &){
+      rval = 1; PSEP_GOTO_CLEANUP("Couldn't push back pair, ");
+    }
+  }
+
+ CLEANUP:
+  if(rval){
+    cerr << "IntervalSet constructor failed\n";
+    interval_list.clear();
+  }
+}
---
+++ setbank.hpp
@@ -0,0 +1,71 @@
+#ifndef PSEP_SETBANK_HPP
+#define PSEP_SETBANK_HPP
+
+#include<unordered_set>
+#include<vector>
+#include<iostream>
+
+#include<boost/functional/hash.hpp>
+
+#include "PSEP_util.hpp"
+
+//namespace PSEP{
+
+void hashing_test();
+void interactive_test();
+
+struct IntervalSet {
+  IntervalSet(std::vector<int> &nodelist,
+	      const std::vector<int> &best_tour_nodes,
+	      const std::vector<int> &perm);
+  IntervalSet(std::vector<IntPair> _interval_list) :
+    interval_list(_interval_list), use_count(0) {}
+  
+  std::vector<IntPair> interval_list;
+  int use_count;
+  
+  explicit operator bool() const { return !(interval_list.empty()); }
+  bool operator==(const IntervalSet &rhs) const {
+    return interval_list == rhs.interval_list;
+  }
+  
+  void print(){
+    for(const IntPair &p : interval_list)
+      std::cout << p.first << ", " << p.second << "\n";
+  }
+};
+
+//this is needed so boost::hash can hash the struct
+std::size_t hash_value(const IntervalSet &S);
+
+typedef std::unordered_set<IntervalSet, boost::hash<IntervalSet>> SetHash;
+typedef SetHash::iterator SetRef;
+
+struct HyperGraph {
+  std::vector<SetRef> set_refs;
+  int rhs;
+  int row_number;
+};
+
+class SetBank {
+  SetBank(std::vector<int> &best_tour_nodes, std::vector<int> &_perm) :
+    tour_nodes(best_tour_nodes), perm(_perm) {}
+
+  void get_hypergraph(HyperGraph &newcut,
+		      std::vector<std::vector<int>> &hypergraph_edges);
+  void delete_hypergraph(HyperGraph &oldcut);  
+  
+private:
+  SetRef add_or_increment(std::vector<int> nodeset);
+  void remove_or_decrement(std::vector<int> nodeset);
+  
+  SetHash set_bank;
+  
+  std::vector<int> tour_nodes;
+  std::vector<int> perm;
+}
+
+
+}
+
+#endif
