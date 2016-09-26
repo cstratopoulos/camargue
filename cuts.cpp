a18bcedfae01f736d01bdc36d5798cd8558ad8a4
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-09-26 17:19:38 -0400

    moved cutqueue implementation

---
+++ cuts.cpp
@@ -0,0 +1,21 @@
+#include "cuts.hpp"
+
+using namespace std;
+using namespace PSEP;
+
+template<>
+void CutQueue<HyperGraph>::push_front(const HyperGraph &H)
+{
+  cut_q.push_front(H);
+  if(cut_q.size() > q_capacity){
+    cut_q.back().delete_refs();
+    cut_q.pop_back();
+  }
+}
+
+template<>
+void CutQueue<HyperGraph>::pop_front()
+{
+  cut_q.front().delete_refs();
+  cut_q.pop_front();
+}
