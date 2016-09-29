79b5b3eba7f98b354d2c6ca804cb333036709019
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-09-29 19:44:13 -0400

    folder for utility and testing scripts

---
+++ scripts/sloc.sh
@@ -0,0 +1,5 @@
+cat $* | # cat input files into pipe
+sed '/^\ *\/\// d' | #delete // style comments
+sed '/^\ *[ \/\\]\*/ d' | #delete /* style comments and \* and /* ' *'
+#sed '/^#include/ d' | #delete include statements
+wc -l
