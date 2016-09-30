81edcb877e4d35a65ba761074c4842709597a8ca
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-09-30 12:55:02 -0400

    fewer trials

--- scripts/cutq.sh
+++ scripts/cutq.sh
@@ -3,7 +3,7 @@ probname=$2 #second argument is problem name with no .tsp suffix
 rawtimes="$probname"_q"$qsize".txt
 >"$rawtimes"
 
-for i in 1 2 3 4 5; do
+for i in 1 2; do
     ../PSEP -S -s 1000 -c 1 -q "$qsize" ../problems/"$probname".tsp |
 	grep 'blossom sep' >> "$rawtimes"
 done
@@ -11,11 +11,8 @@ done
 sed -i.back 's/.*ratio. //g' "$rawtimes"
 rm *.back
 
-#cat "$rawtimes" | tr -d -c '[.0-9\n]' > "$rawtimes"tmp.txt
-#mv "$rawtimes"tmp.txt "$rawtimes"
-
 sumvar=$(paste -s -d + "$rawtimes")
-avg=$(echo "($sumvar)/5" | bc -l)
+avg=$(echo "($sumvar)/2" | bc -l)
 printf "%s %.3d avg %.6f\n" "$probname" "$qsize" "$avg" \
        >> "$probname"_q_reports.txt
 
