352ec1abfddfb9954fb5154e1ba0bbad0d260a59
christos2 <c.stratopoulos@gmail.com>
2016-10-07 13:58:59 -0400

    changed default from 1 per round

--- PSEP.cpp
+++ PSEP.cpp
@@ -50,7 +50,7 @@ static int initial_parse(int ac, char **av, string &fname,
   bool rand = false;
   int pricing_choice = 0;
   int dp_factor = -1;
-  int cuts_per_round = 1;
+  int cuts_per_round = 4;
   int max_q_size = 150;
   int seed = 0;
   int ncount = 0;
