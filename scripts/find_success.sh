f31060e21d7c2c3abda3fa9ed8cea6a341e32307
christos2 <c.stratopoulos@gmail.com>
2016-10-10 17:47:04 -0400

    changed trap thing

--- scripts/find_success.sh
+++ scripts/find_success.sh
@@ -1,15 +1,16 @@
 PID=$$
 probname=$1
-rawdata="$probname"_"$PID"
+rawdata="$probname"_"$PID"_raw
 >"$rawdata".txt
+
 
 ../PSEP -Sc2 -u10  ../problems/"$1".tsp >> "$rawdata".txt 2>&1
 
-opt=$(grep -i 'optimal' "$rawdata".txt | wc -l)
+opt=$(grep -i 'fathomed optimal' "$rawdata".txt | wc -l)
 
 if [ "$opt" -ge 1 ]
 then
-    mv "$rawdata".txt "$rawdata"_success.txt
+    mv "$rawdata".txt "$probname"_"$PID"_success.txt
     echo "Successful run!"
     echo -n $'\a'
     echo -n $'\a'
