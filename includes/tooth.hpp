00b33ac274875fe518eba9c1bb8c77777c4cfac6
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-11-06 16:10:35 -0500

    documentation

--- includes/tooth.hpp
+++ includes/tooth.hpp
@@ -25,25 +25,28 @@
 
 namespace PSEP {
 
-/*
- * Structure for storing the simple teeth which may comprise simple domino
- * parity inequalities. A simple tooth is a root node and a segment of a
+/** Structure for storing candidate simple teeth.
+ * This structure holds a candidate simple tooth; a collection of these may
+ * potentially comprise a simple domino parity inequalities. A simple tooth is
+ * a root node, \p root, and a segment of a
  * best known tour, stored as best tour node indices. Hence the root 
- * node is best_tour_nodes[root], and the segment is
- * best_tour_nodes[body_start], best_tour_nodes[body_start + 1], ...
- *                                best_tour_nodes[body_end],
- * incrementing indices mod ncount. 
+ * node is `best_tour_nodes[root]`, and the segment is
+ * `best_tour_nodes[body_start], best_tour_nodes[body_start + 1], ...
+ *                                best_tour_nodes[body_end]`,
+ * incrementing indices mod the number of nodes in the graph.
  * A SimpleTooth is meaningless without management by its CandidateTooth
  * class, which is responsible for all nontrivial operations on the tooth. 
  */
 
 struct SimpleTooth {
   SimpleTooth() = default;
-  
+
+  /** Constructs a SimpleTooth with root and body segment. */
   SimpleTooth(int _root, int _body_start, int _body_end) :
     root(_root), body_start(_body_start), body_end(_body_end),
     slack(INFINITY) {}
 
+  /** Constructs a SimpleTooth with root, body segment, and slack. */
   SimpleTooth(int _root, int _body_start, int _body_end, double _slack) :
     root(_root), body_start(_body_start), body_end(_body_end),
     slack(_slack) {}
@@ -59,39 +62,44 @@ struct SimpleTooth {
   int body_start;
   int body_end;
 
+  /** The slack of the simple tooth inequality. 
+   * Suppose the tooth has \p root \f$ i \f$, and body \f$ S \f$ equal to 
+   * `best_tour_nodes[body_start]`, `best_tour_nodes[body_start + 1]`, ...,
+   * `best_tour_nodes[body_end]`.
+   * If \f$ x^* \f$ is the %LP solution, then \p slack is \f[
+   * 2x(E(S)) + x(E(i:S)) \le 2 |S| - 1. \f]
+   */
   double slack;
 
-  int cutgraph_index;
+  int cutgraph_index; /** Labelling index for use in DPCutGraph. */
 
   //returns true iff root lies in the middle of the segment from body_start
   // to body_end
   bool sandwich() const;
 
-  //returns true iff the body segment of the tooth contains node_index
+  /** Returns true iff the body segment of the tooth contains \p node_index. */
   bool body_contains(const int node_index) const;
 
-  //returns true iff R has the same root as this tooth, and this tooth's
-  //body is a subset of R's body
+  /** Returns true if this tooth's body is a subset of \p R's body. */
   bool is_subset_of(const SimpleTooth &R) const;
 };
 
-/*
- * Structure for representing bodies of simple teeth.
- * start/end are specified relative to a vector of tour nodes, so the segment
- * is
- * tour_nodes[cut_start], tour_nodes[cut_start + 1], ... , tour_nodes[cut_end]
- * slack is the slack of the SEC associated to the segment
+/** Structure for representing bodies of simple teeth.
+ * \p start and \p end are specified relative to a vector of tour nodes, 
+ * so the segment is
+ * `tour_nodes[cut_start], tour_nodes[cut_start + 1], ... , tour_nodes[cut_end]`
  */
 struct tooth_seg {
+  /** Construct a segment with given \p start, \p end, and \p slack. */
   tooth_seg(int _start, int _end, double _slack) :
     start(_start), end(_end), slack(_slack) {}
 
   int start;
   int end;
-  double slack;
+  double slack; /** The slack of the SEC associated to the segment. */
 };
 
-/*
+/** Class for generating and managing a collection of candidate SimpleTeeth.
  * This class generates and manages a collection of candidate teeth to be 
  * included in a simple domino parity inequality. It is also responsible for
  * nontrivial operations on teeth which require knowledge of the best tour
@@ -101,6 +109,7 @@ struct tooth_seg {
 class CandidateTeeth {
 public:
   CandidateTeeth() = default;
+  
   CandidateTeeth(std::vector<int> &_delta, std::vector<int> &_edge_marks,
 		 std::vector<int> &_best_tour_nodes,
 		 std::vector<int> &_perm,
@@ -109,22 +118,24 @@ public:
 		 std::vector<int> &_support_elist,
 		 std::vector<double> &_support_ecap);
 
+  /** The number of nodes in \p T's body. */
   int body_size(const SimpleTooth &T);
 
-  //if T has root i, body S, T complement has root i, body V\(Su{i})
+  /** The relative complement of a SimpleTooth.
+   * If \p T has root \f$ i \f$ and body \f$ S \f$, then `complement(T)` has
+   * root \f$ i \f$ and body \f$ V \setminus (S cup \left{ i \right}) \f$.
+   */
   void complement(SimpleTooth &T);
 
-  /*
-   * For teeth T, R with the same root, returns whether the body of T is a
-   * subset of the body of R, storing the result in result.
-   * TODO: generalize to arbitrary teeth
+  /** Is the body of \p T a subset of the body of \p R.
+   * For teeth \p T, \p R with the same \p root, returns whether the body of
+   * \p T is a subset of the body of \p R, storing the result in \p result.
+   * @returns 0 if success, 1 if failure.
+   * @todo generalize to arbitrary teeth.
    */
   int body_subset(const SimpleTooth &T, const SimpleTooth &R, bool &result);
 
-  /*
-   * Build a collection of light teeth: teeth for which the slack of the 
-   * associated simple tooth inequality is less than 1/2
-   */
+  /** Build a collection of light simple teeth. */
   int get_light_teeth();
 
   void print_tooth(const SimpleTooth &T);
