8c84a9d444d942bfcbfbd0659be5856eadfa23c4
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-11-09 12:28:45 -0500

    used c io instead

--- source/timer.cpp
+++ source/timer.cpp
@@ -54,24 +54,20 @@ void Timer::report(bool show_cpu)
   
   printf("%.6fs wall \t", wall_elapsed.count());
   if(ratio_timer)
-    cout << "("
-	 << setprecision(2)
-	 << (100 * (wall_elapsed.count() / ratio_timer->wall_elapsed.count()))
-	 << setprecision(6)
-	 <<  "% of " << ratio_timer->timer_name << ")";
-  cout << "\n";
+    printf("(%04.1f%% of %s)",
+	   (100 * (wall_elapsed.count() / ratio_timer->wall_elapsed.count())),
+	   ratio_timer->timer_name.c_str());
+  printf("\n");
 
   if(show_cpu){
     string cpu_pad(22, ' ');
     cout << cpu_pad;
     printf("%.6fs CPU \t", cpu_elapsed);
     if(ratio_timer)
-      cout << "("
-	   << setprecision(2)
-	   << (100 * (cpu_elapsed / ratio_timer->cpu_elapsed))
-	   << setprecision(6)
-	   <<  "% of " << ratio_timer->timer_name << ")";
-    cout << "\n";
+      printf("(%04.1f%% of %s)",
+	     (100 * (cpu_elapsed / ratio_timer->cpu_elapsed)),
+	     ratio_timer->timer_name.c_str());
+    printf("\n");
   }
 }
 
