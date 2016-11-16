a644d8cd6ab51c6a9c95b4568eb5d17de649575b
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-11-15 23:28:16 -0500

    added tiny tests with examples from paper

--- source/tests/dp_test.cpp
+++ source/tests/dp_test.cpp
@@ -1,50 +1,74 @@
 #include "tests.hpp"
 #include "datagroups.hpp"
-#include "cutcall.hpp"
+#include "tooth.hpp"
+#include "DPgraph.hpp"
 
 #include <vector>
 #include <string>
+#include <iostream>
 
-#ifdef PSEP_DO_TESTS
-
-#define CATCH_CONFIG_MAIN
 #include <catch.hpp>
 
+using std::cout;
 using std::vector;
 using std::string;
 
-TEST_CASE("Concorde subtour solutions are in subtour poly", "[subtour]") {
-  int rval = 0;
-  bool result;
-  PSEP::LP::Prefs prefs;
+#ifdef PSEP_DO_TESTS
 
-  vector<string> probs{"dantzig42", "pr1002", "lin105", "rl1304", "d2103",
-		       "fleisA9", "fleisB9"};
+TEST_CASE ("Toy examples from paper",
+	   "[simpleDP]") {
+  vector<string> probs{"fleisA9", "fleisB9"};
 
-  for(string &fname : probs){
-    SECTION(fname){
-      string
-	probfile = "problems/" + fname + ".tsp",
-	solfile = "test_data/tours/" + fname + ".sol",
-	subtourfile = "test_data/subtour_lp/" + fname + ".sub.x";
-      PSEP::Data::GraphGroup g_dat;
-      PSEP::Data::BestGroup b_dat;
-      PSEP::Data::SupportGroup s_dat;
-      std::vector<double> lp_edges;
-      rval = PSEP::Data::make_cut_test(probfile, solfile, subtourfile,
-				       g_dat, b_dat, lp_edges, s_dat);
-      REQUIRE(rval == 0);
+    for(string &fname : probs){
+      SECTION(fname){
+	string
+	  probfile = "problems/" + fname + ".tsp",
+	  solfile = "test_data/tours/" + fname + ".sol",
+	  subtourfile = "test_data/subtour_lp/" + fname + ".sub.x";
+	PSEP::Data::GraphGroup g_dat;
+	PSEP::Data::BestGroup b_dat;
+	PSEP::Data::SupportGroup s_dat;
+	std::vector<double> lp_edges;
+	
+	REQUIRE_FALSE(PSEP::Data::make_cut_test(probfile, solfile, subtourfile,
+						g_dat, b_dat, lp_edges,
+						s_dat));
+	cout << "Best tour: ";
+	for(int i : b_dat.best_tour_nodes) cout << i << ", "; cout << "\n";
 
-      PSEP::Data::LPGroup lp_dat(g_dat.m_graph, prefs, b_dat.perm);
-      lp_dat.m_lp_edges = lp_edges;
+	  
+	PSEP::CandidateTeeth cands(g_dat.delta, g_dat.edge_marks,
+				   b_dat.best_tour_nodes, b_dat.perm,
+				   s_dat.G_s, s_dat.support_elist,
+				   s_dat.support_ecap);
+	
+	REQUIRE_FALSE(cands.get_light_teeth());
 
-      PSEP::CutControl controlla(g_dat, b_dat, lp_dat, s_dat, nullptr);
+	PSEP::DPCutGraph dp_graph(cands.light_teeth, b_dat.perm, s_dat.G_s);
+	PSEP::CutQueue<PSEP::dominoparity> dp_q;
 
-      rval = controlla.in_subtour_poly(result);
-      REQUIRE(rval == 0);
-      REQUIRE(result == true);
+	REQUIRE(dp_graph.simple_DP_sep(dp_q) == 0);
+
+	while(!dp_q.empty()){
+	  PSEP::dominoparity dp = dp_q.peek_front();
+	  vector<int> bt = b_dat.best_tour_nodes;
+
+	  cout << "Found dp with....\n";
+	  cout << "Handle: ";
+	  for(int i : dp.degree_nodes) cout << bt[i] << ", "; cout << "\n";
+	  cout << "Nonneg edges: ";
+	  for(IntPair &e : dp.nonneg_edges)
+	    cout << "(" << bt[e.first] << ", " << bt[e.second] << "), ";
+	  cout << "\n";
+	  cout << "Simple teeth:\n";
+	  for(vector<PSEP::SimpleTooth::Ptr> &vec : cands.light_teeth)
+	    for(PSEP::SimpleTooth::Ptr &T : vec)
+	      cands.print_tooth(*T, true);
+
+	  dp_q.pop_front();
+	}
+      }
     }
-  }
 }
 
-#endif  //PSEP_DO_TESTS
+#endif //PSEP_TEST_TOOTH
