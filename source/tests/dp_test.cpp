caa31434d4ee3ccd074341210c6b97b0815fceaa
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-11-17 11:25:19 -0500

    added tests for normal size isntances

--- source/tests/dp_test.cpp
+++ source/tests/dp_test.cpp
@@ -15,10 +15,12 @@ using std::string;
 
 #ifdef PSEP_DO_TESTS
 
-TEST_CASE ("Toy examples from paper",
-	   "[simpleDP]") {
-  vector<string> probs{"fleisA9", "fleisB9", "comb9", "ulysses16",
-		       "dantzig42"};
+//for these tests, consider defining PSEP_DO_TESTS in DPgraph.hpp to generate
+//a DOT file for the witness cutgraph which can be viewed with graphviz or
+//some such
+TEST_CASE ("Tiny simple DP cutgraph tests",
+	   "[simpleDP][tiny][cutgraph]") {
+  vector<string> probs{"fleisA9", "fleisB9", "comb9", "ulysses16"};
 
     for(string &fname : probs){
       SECTION(fname){
@@ -48,25 +50,33 @@ TEST_CASE ("Toy examples from paper",
 	
 	for(vector<PSEP::SimpleTooth::Ptr> &vec : cands.light_teeth)
 	  for(PSEP::SimpleTooth::Ptr &T : vec){
-	    cout << "Root label: " << cands.print_label(*T, true) << "\n";
+	    cands.print_tooth(*T, true);
 	  }
 
-	PSEP::DPCutGraph dp_graph(fname, cands.light_teeth, cands,
+	PSEP::DPCutGraph dp_graph(
+#ifdef PSEP_DO_VIZ
+				  fname,
+#endif
+				  cands.light_teeth, cands,
 				  b_dat.perm, s_dat.G_s,
 				  s_dat.support_elist, s_dat.support_ecap);
 	PSEP::CutQueue<PSEP::dominoparity> dp_q;
 
-	REQUIRE(dp_graph.simple_DP_sep(dp_q) == 0);
+	if(fname == "ulysses16")
+	  REQUIRE(dp_graph.simple_DP_sep(dp_q) == 2);
+	else
+	  REQUIRE(dp_graph.simple_DP_sep(dp_q) == 0);
 
 	while(!dp_q.empty()){
 	  PSEP::dominoparity dp = dp_q.peek_front();
+	  vector<int> &bt = b_dat.best_tour_nodes;
 
 	  cout << "Found dp with....\n";
 	  cout << "Handle: ";
-	  for(int i : dp.degree_nodes) cout << i << ", "; cout << "\n";
+	  for(int i : dp.degree_nodes) cout << bt[i] << ", "; cout << "\n";
 	  cout << "Nonneg edges: ";
 	  for(IntPair &e : dp.nonneg_edges)
-	    cout << "(" << e.first << ", " << e.second << "), ";
+	    cout << "(" << bt[e.first] << ", " << bt[e.second] << "), ";
 	  cout << "\n";
 	  cout << "Simple teeth: (" << dp.used_teeth.size() << " total)\n";
 	  for(PSEP::SimpleTooth *T : dp.used_teeth)
@@ -78,5 +88,46 @@ TEST_CASE ("Toy examples from paper",
       }
     }
 }
+
+//these tests on medium sized examples should not be done with DO_VIZ. 
+#ifndef PSEP_DO_VIZ
+TEST_CASE ("simple DP cutgraph tests",
+	   "[simpleDP][cutgraph]") {
+  vector<string> probs{"d2103", "pr1002", "rl1304"};
+
+    for(string &fname : probs){
+      SECTION(fname){
+	string
+	  probfile = "problems/" + fname + ".tsp",
+	  solfile = "test_data/tours/" + fname + ".sol",
+	  subtourfile = "test_data/subtour_lp/" + fname + ".sub.x";
+	PSEP::Data::GraphGroup g_dat;
+	PSEP::Data::BestGroup b_dat;
+	PSEP::Data::SupportGroup s_dat;
+	std::vector<double> lp_edges;
+	
+	REQUIRE_FALSE(PSEP::Data::make_cut_test(probfile, solfile, subtourfile,
+						g_dat, b_dat, lp_edges,
+						s_dat));
+	
+	PSEP::CandidateTeeth cands(g_dat.delta, g_dat.edge_marks,
+				   b_dat.best_tour_nodes, b_dat.perm,
+				   s_dat.G_s, s_dat.support_elist,
+				   s_dat.support_ecap);
+	
+	REQUIRE_FALSE(cands.get_light_teeth());
+	cands.weak_elim();
+      
+	PSEP::DPCutGraph dp_graph(cands.light_teeth, cands,
+				  b_dat.perm, s_dat.G_s,
+				  s_dat.support_elist, s_dat.support_ecap);
+	PSEP::CutQueue<PSEP::dominoparity> dp_q;
+	
+	REQUIRE(dp_graph.simple_DP_sep(dp_q) == 0);
+	cout << "\n";
+      }
+    }
+}
+#endif //PSEP_DO_VIZ
 
 #endif //PSEP_TEST_TOOTH
