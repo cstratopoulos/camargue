e0c77b8ab32c7eb9019c0641f9607ffb8cf16b23
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-11-21 15:30:48 -0500

    cleaned up scripts

--- scripts/cutq_wrapper_big.sh
+++
@@ -1,9 +0,0 @@
-for prob; do
-    for qcap in 50 100 150 250; do
-	./cutq.sh "$qcap" "$prob"
-    done
-
-    sort -nk3 "$prob"_ratios.txt -o "$prob"_ratios.txt
-    sort -nk3 "$prob"_times.txt -o "$prob"_times.txt
-    rm "$prob"_q*.txt
-done
--- scripts/cutsper_wrapper.sh
+++
@@ -1,9 +0,0 @@
-for prob; do
-    for maxround in 1 2 4 8 12 16 25 50; do
-	./cutsper.sh "$maxround" "$prob"
-    done
-
-    for suff in _pivratios.txt _soltimes.txt _failratios.txt; do
-	sort -nk3 "$prob""$suff" -o "$prob""$suff"
-    done
-done
