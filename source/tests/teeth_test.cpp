9e0be917480dcf498d1e8e27a334b8af4d046e47
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-11-23 22:04:21 -0500

    tests for new linsub callback

--- source/tests/teeth_test.cpp
+++ source/tests/teeth_test.cpp
@@ -31,8 +31,110 @@ static int dump_segment(double cut_val, int cut_start, int cut_end,
   return 0;
 }
 
-TEST_CASE("New tooth constructor tests", "[tooth]") {
-  vector<string> tests{"fleisA9", "fleisB9", "comb9"};
+TEST_CASE("New candidate teeth with elim",
+	  "[tooth][tiny]") {
+  vector<string> tests{"fleisA9", "fleisB9", "comb9", "ulysses16",
+		       "dantzig42", "pr76",
+		       "d2103", "pr1002", "rl1304"};
+
+  for(string &fname : tests){
+    SECTION(fname){
+      string
+	probfile = "problems/" + fname + ".tsp",
+	solfile = "test_data/tours/" + fname + ".sol",
+	subtourfile = "test_data/subtour_lp/" + fname + ".sub.x";
+      PSEP::Data::GraphGroup g_dat;
+      PSEP::Data::BestGroup b_dat;
+      PSEP::Data::SupportGroup s_dat;
+      std::vector<double> lp_edges;
+	
+      REQUIRE_FALSE(PSEP::Data::make_cut_test(probfile, solfile, subtourfile,
+					      g_dat, b_dat, lp_edges,
+					      s_dat));
+      int ncount = s_dat.G_s.node_count;
+      
+      if(ncount < 20){
+	cout << "Best tour:\n";
+	for(int i : b_dat.best_tour_nodes)
+	  cout << " " << i << ", ";
+	cout << "\n";
+      }
+
+      double ft = PSEP::zeit();
+      PSEP::nu::CandidateTeeth cands(g_dat, b_dat, s_dat);
+      REQUIRE_FALSE(cands.get_light_teeth());
+      ft = PSEP::zeit() - ft;
+      int numfound = 0;
+
+      cout << "\tLEFT ADJACENT TEETH\n";
+      for(vector<PSEP::nu::SimpleTooth::Ptr> &vec : cands.right_teeth){
+	numfound += vec.size();
+	if(ncount < 100)
+	  for(const PSEP::nu::SimpleTooth::Ptr &T : vec){
+	    cands.print_tooth(*T, ncount < 20);
+	  }
+      }
+
+      cout << "\tRIGHT ADJACENT TEETH\n";
+      for(vector<PSEP::nu::SimpleTooth::Ptr> &vec : cands.left_teeth){
+	numfound += vec.size();
+	if(ncount < 100)
+	  for(const PSEP::nu::SimpleTooth::Ptr &T : vec){
+	    cands.print_tooth(*T, ncount < 20);
+	  }
+      }
+
+      cout << "\tDISTANT TEETH\n";
+      for(vector<PSEP::nu::SimpleTooth::Ptr> &vec : cands.dist_teeth){
+	numfound += vec.size();
+	if(ncount < 100)
+	  for(const PSEP::nu::SimpleTooth::Ptr &T : vec){
+	    cands.print_tooth(*T, ncount < 20);
+	  }
+      }
+      cout << "Found " << numfound << " in " << ft << "s\n";
+      int
+	sum_sums = 0, sum_lefts = 0, sum_rights = 0, max_sum = 0,
+	max_left = 0, max_right = 0;
+      for(int i = 0; i < ncount; ++i){
+	int left_sum = cands.left_teeth[i].size();
+	int right_sum = cands.right_teeth[i].size();
+	int sum = left_sum + right_sum;
+	
+	sum_sums += sum;
+	sum_lefts += left_sum;
+	sum_rights += right_sum;
+	if(sum > max_sum) max_sum = sum;
+	if(left_sum > max_left) max_left = left_sum;
+	if(right_sum > max_right) max_right = right_sum;
+      }
+      cout << "Avg sum of sizes of left and right teeth w same root: "
+	   << ((double) sum_sums / (double) ncount) << ", biggest: "
+	   << max_sum << "\n";
+      cout << "Avg num left teeth: "
+	   << ((double) sum_lefts / (double) ncount) << ", max: "
+	   << max_left << "\n";
+      cout << "Avg num right teeth: "
+	   << ((double) sum_rights / (double) ncount) << ", max: "
+	   << max_right << "\n";
+
+      int sum_dists = 0, max_dist = 0;
+      for(auto &vec : cands.dist_teeth){
+	sum_dists += vec.size();
+	if(vec.size() > max_dist)
+	  max_dist = vec.size();
+      }
+      cout << "Avg num dist teeth: "
+	   << ((double) sum_dists / (double) ncount) << ", max: "
+	   << max_dist << "\n";
+      cout << "\n";
+    }
+  }
+}
+
+TEST_CASE("New tooth constructor tests",
+	  "[.tooth][tiny]") {
+  vector<string> tests{"fleisA9", "fleisB9", "comb9", "ulysses16"};
   
   for(string &fname : tests){
     SECTION(fname){
@@ -50,78 +152,97 @@ TEST_CASE("New tooth constructor tests", "[tooth]") {
 					      s_dat));
       PSEP::SupportGraph &G_s = s_dat.G_s;
       int max_deg = 0;
-
-      cout << "Best tour:\n";
-      for(int i : b_dat.best_tour_nodes){
-	cout << i << ", "; 
-	if(G_s.nodelist[i].s_degree > max_deg)
-	  max_deg = G_s.nodelist[i].s_degree;
-      }
-      cout << "\n";
-
-      for(int acc = 0; acc < max_deg; ++acc){
-	for(int i : b_dat.best_tour_nodes){
-	  if(acc < G_s.nodelist[i].s_degree)
-	    cout << G_s.nodelist[i].adj_objs[acc].other_end << "  ";
-	  else
-	    cout << "   ";
-	}
-	cout << "\n";
-      }
-      cout << "\n";
-
       PSEP::nu::CandidateTeeth cands(g_dat, b_dat, s_dat);
       int ncount = g_dat.m_graph.node_count;
 
-      if(ncount <= 20)
-	for(vector<int> &vec : cands.adj_zones){
-	  for(int i : vec)
-	    cout << fabs(i) << "  ";
+      if(ncount <= 20){
+	cout << "Best tour:\n";
+	for(int i : b_dat.best_tour_nodes){
+	  cout << " " << i << ", "; 
+	  if(G_s.nodelist[i].s_degree > max_deg)
+	    max_deg = G_s.nodelist[i].s_degree;
+	}
+	cout << "\n";
+
+	for(int acc = 0; acc < max_deg; ++acc){
+	  for(int i : b_dat.best_tour_nodes){
+	    if(acc < G_s.nodelist[i].s_degree)
+	      cout <<  " " << G_s.nodelist[i].adj_objs[acc].other_end << "  ";
+	    else
+	      cout << "    ";
+	  }
 	  cout << "\n";
 	}
+	cout << "\n";
+
+	for(vector<int> &vec : cands.adj_zones){
+	  for(int i : vec)
+	    cout << setw(2) << i << "  ";
+	  cout << "\n";
+	}
+      }
 
       vector<PSEP::nu::tooth_seg> seg_vec;
       vector<int> &tour = b_dat.best_tour_nodes;
       vector<int> &perm = b_dat.perm;
+      vector<int> endmark(ncount, CC_LINSUB_BOTH_END);
       
       REQUIRE_FALSE(CCcut_linsub_allcuts(G_s.node_count, G_s.edge_count,
 					 &b_dat.best_tour_nodes[0],
-					 &cands.endmark[0],
+					 &endmark[0],
 					 &s_dat.support_elist[0],
 					 &s_dat.support_ecap[0],
 					 2.999, &seg_vec,
 					 dump_segment));
 
+      vector<vector<int>> &zones = cands.adj_zones;
       for(auto s1 = seg_vec.begin(); s1 != seg_vec.end() - 1; ++s1){
 	for(auto s2 = s1 + 1; s2 != seg_vec.end(); ++s2){
 	  PSEP::nu::tooth_seg seg1 = *s1, seg2 = *s2;
 	  
 	  int min_start = fmin(seg1.start, seg2.start);
 	  int max_end = fmin(seg1.end, seg2.end);
-	  PSEP::nu::tooth_seg blanket(min_start, max_end, 0);
 
 	  for(int root = 0; root < ncount; ++root){
-	    if(blanket.contains(root)) continue;
+	    if(seg1.contains(root) || seg2.contains(root)) continue;
 	    
 	    bool zone_equiv = cands.root_equivalent(root, seg1, seg2);
 	    bool brute_equiv = true;
 	    int actual_vx = tour[root];
 	    PSEP::SNode vx = G_s.nodelist[actual_vx];
 
-	    for(int d = 0; d < vx.s_degree; ++d){
-	      int end = perm[vx.adj_objs[d].other_end];
-	      if(seg1.contains(end) != seg2.contains(end)){
+	    int d = 0, end1 = -1;
+	    for(d = 0; d < vx.s_degree; ++d){
+	      end1 = perm[vx.adj_objs[d].other_end];
+	      if(seg1.contains(end1) != seg2.contains(end1)){
 		brute_equiv = false;
 		break;
 	      }
 	    }
 	    
 	    CHECK(zone_equiv == brute_equiv);
-	    if(zone_equiv != brute_equiv)
+	    if(zone_equiv != brute_equiv){
 	      cout << "^^Considering root " << root << ", bodies ("
 		   << seg1.start << ", "
 		   << seg1.end << ") and (" << seg2.start << ", " << seg2.end
-		   << ")^^\n\n";
+		   << ")^^\n";
+	      cout << "Actual: " << tour[root] << " -- ( " << tour[seg1.start]
+		   << ", " << tour[seg1.end] << ") " << "( "
+		   << tour[seg2.start] << ", " << tour[seg2.end] << ")\n";
+	      cout << "Examing delta d = " << d
+		   << " of root " << root << ", actual: "
+		   << actual_vx << "\n";
+	      cout << "Found disagreement with end index " << end1
+		   << ", actual: " << vx.adj_objs[d].other_end << "\n";
+	      cout << "seg1 contains: " << seg1.contains(end1) << ", seg 2: "
+		   << seg2.contains(end1) << "\n";
+	      IntPair s1_range, s2_range;
+	      cands.get_range(root, seg1, s1_range, zones);
+	      cands.get_range(root, seg2, s2_range, zones);
+	      cout << "seg1 range: " << s1_range.first << ", "
+		   << s1_range.second << " seg2 range: "
+		   << s2_range.first << ", " << s2_range.second << "\n\n";
+	    }
 	  }
 	}
       }
