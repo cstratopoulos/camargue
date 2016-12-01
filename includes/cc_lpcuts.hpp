4f89bef6f4433d4b13db52493a11a1a5abdbc052
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-12-01 01:04:41 -0500

    added documentation and prototypes

--- includes/cc_lpcuts.hpp
+++ includes/cc_lpcuts.hpp
@@ -1,3 +1,9 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/** @file
+ * @brief WRAPPERS FOR CONCORDE LP STRUCTURES
+ * 
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #ifndef PSEP_CC_LPCUTS_HPP
 #define PSEP_CC_LPCUTS_HPP
 
@@ -6,22 +12,41 @@ extern "C" {
 }
 
 namespace PSEP {
+
+/** Classes and functions related to cut separation. */
 namespace Cut {
 
+/** Wrapper for Concorde CCtsp_lpcut_in structure. 
+ * This class is meant to provide an extremely limited (but memory safe!) 
+ * interface, for no other purpose but to call certain cut separation routines
+ * provided by Concorde. 
+ * @
+ */
 class CCwrapper {
 public:
   CCwrapper();
   ~CCwrapper();
 
-  int cut_count() const { return _cutcount;}
+  /** Deletes all non-primal cuts.
+   * @pre This function assumes that Concorde heuristic routines were called
+   * with the nodes permuted according to the current best tour.
+   */
+  void filter_primal(); 
+
+  int cut_count() const { return _cutcount; }
+  bool empty() const { return _cutcount == 0; }
   
+  /** Passes address of member pointer for use by separation routines. */
   CCtsp_lpcut_in** pass_ptr() { return &cc_cut; }
+
+  /** Passes address of cut count for use by separation routines. */
   int* count_ptr() { return &_cutcount; }
   
 private:
-  CCtsp_lpcut_in *cc_cut;
-  int _cutcount;
+  CCtsp_lpcut_in *cc_cut; /**< The raw pointer to the Concorde struct. */
+  int _cutcount; /**< Number of cuts in the linked list starting at cc_cut. */
 };
+
 
 }
 }
--- source/tests/fastblossom_test.cpp
+++ source/tests/fastblossom_test.cpp
@@ -1,4 +1,3 @@
-#include "cc_fastblossoms.hpp"
 #include "tests.hpp"
 #include "datagroups.hpp"
 
@@ -22,84 +21,5 @@ using std::string;
 using std::pair;
 
 #ifdef PSEP_DO_TESTS
-
-TEST_CASE("Fast blossosm via wrapper class",
-	  "[fast2m][tiny]"){
-  vector<string> tests{"blossom6", "comb9"};
-
-  for(string &fname : tests){
-    SECTION(fname){
-      string
-	probfile = "problems/" + fname + ".tsp",
-	solfile = "test_data/tours/" + fname + ".sol",
-	subtourfile = "test_data/subtour_lp/" + fname + ".sub.x";
-      PSEP::Data::GraphGroup g_dat;
-      PSEP::Data::BestGroup b_dat;
-      PSEP::Data::SupportGroup s_dat;
-      std::vector<double> lp_edges;
-	
-      REQUIRE_FALSE(PSEP::Data::make_cut_test(probfile, solfile, subtourfile,
-					      g_dat, b_dat, lp_edges,
-					      s_dat));
-
-      PSEP::cc::FastBlossom fb_sep(g_dat, b_dat, s_dat);
-      REQUIRE(fb_sep.cutcall() == PSEP::CutFound::Yes);
-    }
-  }
-}
-
-TEST_CASE("Fast blossoms via concorde", "[fast2m]"){
-  vector<string> tests{"blossom6"};
-
-  for(string &fname : tests){
-    SECTION(fname){
-      string
-	probfile = "problems/" + fname + ".tsp",
-	solfile = "test_data/tours/" + fname + ".sol",
-	subtourfile = "test_data/subtour_lp/" + fname + ".sub.x";
-      PSEP::Data::GraphGroup g_dat;
-      PSEP::Data::BestGroup b_dat;
-      PSEP::Data::SupportGroup s_dat;
-      std::vector<double> lp_edges;
-	
-      REQUIRE_FALSE(PSEP::Data::make_cut_test(probfile, solfile, subtourfile,
-					      g_dat, b_dat, lp_edges,
-					      s_dat));
-      int ncount = s_dat.G_s.node_count;
-      int ecount = s_dat.G_s.edge_count;
-      int cutcount = 0;
-      CCtsp_lpcut_in *cc_cut = nullptr;
-      CCtsp_init_lpcut_in(cc_cut);
-
-      vector<int> cut_elist = s_dat.support_elist;
-            for(int &i : cut_elist) i = b_dat.perm[i];
-
-      REQUIRE_FALSE(CCtsp_fastblossom(&cc_cut, &cutcount, ncount, ecount,
-      				      cut_elist.data(),
-      				      s_dat.support_ecap.data()));
-      REQUIRE(cutcount == 2);
-      int i = 0;
-      for(CCtsp_lpcut_in *c = cc_cut; c; c = c->next){
-	int cq_count = c->cliquecount;
-	cout << ++i << "th cut, clique count: " << cq_count << "\n";
-	cout << "sense: " << c->sense << ", rhs: " << c->rhs << "\n";
-	for(int j = 0; j < cq_count; ++j){
-	  cout << "\tclique " << j << ":\n";
-	  CCtsp_lpclique *clq = &(c->cliques[j]);
-	  CCtsp_segment *segnodes = clq->nodes;
-	  cout << "\t\tsegcount: " << clq->segcount << "\n";
-	  for(int k = 0; k < clq->segcount; ++k){
-	    cout << "\t\tSeg " << k << ": "
-		 << segnodes[k].lo << ", " << segnodes[k].hi << "\n";
-	  }
-	}
-	cout << "\n";
-      }
-      
-      CCtsp_free_lpcut_in(cc_cut);
-      CC_IFFREE(cc_cut, CCtsp_lpcut_in);
-    }
-  }
-}
 
 #endif
