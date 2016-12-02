9b876da781e7f47b7df4ca12b823f5d1be09456c
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-12-01 23:54:17 -0500

    header with scope guard class

---
+++ includes/err_util.hpp
@@ -0,0 +1,55 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/** @file
+ * @brief ERROR HANDLING CLASSES AND ROUTINES
+ * This file contains classes for creating cleanup objects and exceptions 
+ * derived from standard library runtime error. 
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef PSEP_ERRUTIL_HPP
+#define PSEP_ERRUTIL_HPP
+
+#include <stdexcept>
+#include <string>
+
+namespace PSEP {
+
+/** Code from Andrei Alexandrescu's ScopeGuard11 slides.
+ * This class provides a limited implementation of ScopeGuard. I have not 
+ * bothered with the macro definitions for scope exit, or for operator + on 
+ * a scope guard; all cleanup tasks must be specified upon construction. 
+ */
+template <typename act_type>
+class ScopeGuard {
+public:
+  ScopeGuard(act_type action) :
+    final_action(std::move(action)), active(true) {}
+  ~ScopeGuard() { if(active) final_action(); }
+
+  void dismiss() { active = false; }
+
+  ScopeGuard() = delete;
+  ScopeGuard(const ScopeGuard&) = delete;
+  ScopeGuard& operator=(const ScopeGuard&) = delete;
+  ScopeGuard(ScopeGuard&& rhs) :
+    final_action(std::move(rhs.final_action)),
+    active(rhs.active) {rhs.dismiss(); }
+  
+private:
+  act_type final_action;
+  bool active;
+};
+
+/** Type deduction function for ScopeGuard. 
+ * The suggested usage is `auto cleanup = make_guard([] {cleanup_lambda}); `
+ */
+template <typename act_type>
+ScopeGuard<act_type> make_guard(act_type f)
+{
+  return ScopeGuard<act_type>(std::move(f));
+}
+  
+}
+
+
+#endif
