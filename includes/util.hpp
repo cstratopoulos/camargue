ac3f06d71b8480b0f9f2bfcf04c4aabb2b28cc1a
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-12-05 20:04:47 -0500

    renamed versions of util and main file

---
+++ includes/util.hpp
@@ -0,0 +1,204 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */ /**
+ * @file
+ * @brief UTILITY FUNCTIONS AND MACROS/ENUM CLASSES
+ *
+ * This header contains namespace and enum classes for labels/constants that
+ * are used elsewhere in the code, as well as some very simple structures
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef __CMR_UTIL_H
+#define __CMR_UTIL_H
+
+#include <utility>
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+#include <type_traits>
+
+#include <boost/functional/hash.hpp>
+
+/** Macro for printing error message and going to exit label. 
+ * Prints the message \a message to stderr and goes to CLEANUP. Thus CLEANUP
+ * must have been defined as a labelled section in function scope.
+ */
+#define CMR_GOTO_CLEANUP(message) { std::cerr << message; goto CLEANUP; }
+
+/** Conditional version of #CMR_GOTO_CLEANUP(message).
+ * Only prints the error \a message if \a rval is nonzero.
+ */
+#define CMR_CHECK_RVAL(rval, message) {	\
+    if ((rval)) {				\
+      std::cerr << message;			\
+      goto CLEANUP;				\
+    }						\
+  }
+
+/** Version of #CMR_GOTO_CLEANUP(message) for setting return code.
+ * This macro sets rval to 1, prints \a message to stderr, and goes to 
+ * CLEANUP. Its intended use is in mixing try/catch blocks with retcodes, and
+ * in function calls where subtasks have their own retcodes. For example,
+ * `try { task } catch (...) { CMR_SET_GOTO(rval, "task threw exception"); }`
+ * or `subval = sub_task(); if(subval) CMR_SET_GOTO(rval, "sub_task error")`
+ */
+#define CMR_SET_GOTO(rval, message) {		\
+    rval = 1;					\
+    std::cerr << message;			\
+    goto CLEANUP; }
+
+/** The namespace for this project. */
+namespace CMR {
+
+/** Choices for the execution behavior of PureCut. */
+enum class SolutionProtocol {
+
+  /** Pure cutting plane solution
+   * Indicates attempt to solve solely by pivoting and adding primal cutting
+   * planes, continuing until optimality or failure. 
+   */
+  PURECUT,
+
+  /** Augment-Branch-Cut solution
+   * Indicates that `PureCut.solve` will run until some specified threshold of
+   * time or cuts, and then will be embedded in an Augment-and-Branch-and-Cut
+   * solution process if optimality has not been attained.
+   */
+  ABC
+};
+
+/** A POD struct for creating a randomly generated euclidean TSP instance. */
+struct RandProb {
+  RandProb() : nodecount(0), gridsize(100), seed(0) {}
+  int nodecount; /**< Number of cities in the problem. */
+  int gridsize; /**< Size of grid from which coordinates will be drawn. */
+  int seed; /**<Random seed for generation of points. */
+};
+
+/** A POD struct for preferences related to tour file output.
+ * This structure stores preferences about the types of files (if any) that
+ * will be saved to disk during the solution process. Existing files will
+ * be overwritten; it is the responsibility of the calling routine to make
+ * sure that they are hopefully only overwritten by better tours than before!
+ * @todo Add a make GIF type parameter. 
+ */
+struct OutPrefs {
+  OutPrefs() : save_tour(true),
+	       dump_xy(false),
+	       save_tour_edges(false),
+	       probname() {}
+  OutPrefs(const bool _save_tour, const bool _dump_xy,
+	   const bool _save_tour_edges) :
+    save_tour(_save_tour),
+    dump_xy(_dump_xy),
+    save_tour_edges(_save_tour_edges),
+    probname() {}
+  
+  bool save_tour, /**< Save tour nodes to `probname.sol`. */
+    dump_xy, /**< If possible, dump xy-coords to `probname.xy` */
+    save_tour_edges; /**< Save tour edges to `probname_tour.x`. */
+  std::string probname; /**< The name of the problem. */
+};
+
+/** Namespace for classes, constants, and enums related to %LP relaxations. */
+namespace LP {
+
+/** The CPLEX default iteration limit for simplex optimizers */
+constexpr long long DEFAULT_ITLIM = 9223372036800000000;
+
+/** Enum class for primal pricing protocols. 
+ * Pricing is an enum class for methods by which pivot variables may be
+ * selected in the primal simplex method. See CPLEX documentation for more
+ * info. These appear in order of least to most computationally intensive and,
+ * theoretically, least to most effective at identifying non-degenerate pivots.
+ */
+enum class Pricing {
+  Devex, /**< Devex pricing. */
+  SlackSteepest, /**< Steepest edge with slack initial norms. */
+  Steepest /**< True steepest edge. */
+};
+
+/** Enum class for categorizing lp solutions. */
+enum class PivType {
+  Frac, /**< Fractional solution. */
+  Subtour, /**< Integral subtour. */
+  Tour, /**< A new or augmented tour. */
+  FathomedTour /**< A Tour with a dual feasible basis in the current lp. */
+};
+
+/*
+ * Prefs is a simple struct to store preferences for the lp solver.
+ *
+ * price_method is one of the Pricing types indicated above
+ * dp_threshold - controls when simple domino parity inequality separation
+ *    will be called. Negative values mean no simple DP separation, no 
+ *    matter what. Nonnegative values mean simple DP separation will be
+ *    called only after that many rounds of cutting plane generation, or
+ *    if no other cuts are found
+ */
+
+/** POD struct for lp solver preferences. */
+struct Prefs {
+  Prefs();
+  Prefs(Pricing _price, int _dp_threshold, int max_round, int q_max);
+      
+  Pricing price_method; /**< Which primal pricing criterion to use. */
+      
+  int dp_threshold; /**< Wait this many rounds before calling simple DP sep. */
+  int max_per_round; /**< Add at most this many cuts of each type per round. */
+  int q_max_size; /**< Keep a pool of this many blossoms. */
+};
+}
+
+
+/** Namespace for numerical tolerances used in this project. */
+namespace Epsilon {
+
+/** Numbers less than this are treated as zero. */
+constexpr double Zero = 0.000001;
+
+/** Cuts are not considered violated unless violated by at least this much. */
+constexpr double Cut = 0.0001;
+
+/** The connected component tolerance for Grotschel Holland fast blossoms. */
+constexpr double GH = 0.3;
+
+}
+
+/** CPU time function. */
+double zeit (void);
+
+/** Wall-clock time function. */
+double real_zeit (void);
+
+/** Prints every entry of a vector */
+template<typename entry_t>
+void print_vec(std::vector<entry_t> const &vec){
+  for(int i = 0; i < vec.size(); i++)
+    std::cout << "Entry " << i << ": " << vec[i] << "\n";
+}
+
+/** Prints nonzero entries of a vector. */
+template<typename entry_t >
+void print_vec_nonzero(std::vector<entry_t> const &vec){
+  for(int i = 0; i < vec.size(); i++)
+    if(vec[i] != 0)
+      std::cout << "Entry " << i << ": " << vec[i] << "\n";
+}
+
+/** Exception-safe creation of a unique_ptr, from Herb Sutter.
+ * This is a c++14 feature which is ported to c++11 under the CMR namespace,
+ * since it exists as std::make_unique in c++14 and onwards.
+ */
+template<typename T, typename ...Args>
+std::unique_ptr<T> make_unique( Args&& ...args )
+{
+    return std::unique_ptr<T>( new T( std::forward<Args>(args)... ) );
+}
+    
+}
+
+typedef std::pair<int, int> IntPair;
+typedef std::unordered_map<IntPair, int, boost::hash<IntPair>> IntPairMap;
+
+
+#endif
---
+++ source/camargue.cpp
@@ -0,0 +1,269 @@
+#include "tsp_solver.hpp"
+#include "util.hpp"
+#include "graph_io.hpp"
+#include "timer.hpp"
+#include "tests.hpp"
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <getopt.h>
+
+using std::vector;
+using std::string;
+using std::cout;
+
+#ifndef CMR_DO_TESTS
+
+static int initial_parse(int ac, char **av, std::string &fname,
+			 std::string &tour_fname,
+			 CMR::RandProb &randprob, CMR::LP::Prefs &prefs,
+			 CMR::OutPrefs &o_prefs,
+			 bool &sparseflag, int &qnearest);
+
+static void usage(const std::string &fname);
+
+int main(int argc, char* argv[]){
+  CMR::LP::Prefs prefs;
+  CMR::OutPrefs o_prefs;
+  CMR::RandProb randprob;
+  std::unique_ptr<CCdatagroup> dat(new CCdatagroup);
+  /**@todo make this a regular ptr bc it confuses valgrind maybe */
+  std::string probfile, tourfile;
+  bool do_sparse = false;
+  int qnearest = 0;
+  /**@todo probably put this somewhere else */
+
+  if(initial_parse(argc, argv, probfile, tourfile, randprob, prefs, o_prefs,
+  		   do_sparse, qnearest)){
+    std::cerr << "Problem parsing arguments\n";
+    exit(1);
+  }
+
+  std::unique_ptr<CMR::TSPSolver> solver;
+
+  CMR::Timer t("Overall");
+  CMR::Timer c("Constructors", &t);
+  t.start();
+  c.start();
+  
+
+  try {
+    if(tourfile.empty())
+      solver = CMR::make_unique<CMR::TSPSolver>(probfile, randprob,
+						  o_prefs, prefs, dat,
+						  do_sparse, qnearest);
+    else
+      solver = CMR::make_unique<CMR::TSPSolver>(probfile, tourfile,
+						  o_prefs, prefs, dat,
+						  do_sparse, qnearest);
+  } catch(...) {
+    return 1;
+  }
+
+  c.stop();
+      
+  int rval = solver->call(CMR::SolutionProtocol::PURECUT, do_sparse);
+
+  t.stop();
+  c.report(false);
+  t.report(true);
+
+  return rval;
+}
+
+static int initial_parse(int ac, char **av, std::string &fname,
+			 std::string &tourfname,
+			 CMR::RandProb &randprob,
+			 CMR::LP::Prefs &prefs, CMR::OutPrefs &o_prefs,
+			 bool &sparseflag,
+			 int &qnearest){
+  bool rand = false;
+  int pricing_choice = 0;
+  int dp_factor = -1;
+  int cuts_per_round = 4;
+  int max_q_size = 150;
+  int seed = 0;
+  int ncount = 0;
+  int gridsize = 100;
+  int tourprefs = 0;
+  qnearest = 0;
+
+  int c;
+
+  if(ac == 1){
+    usage(av[0]);
+    return 1;
+  }
+
+  while((c = getopt(ac, av, "ad:c:q:p:RSXg:n:s:t:u:o:")) != EOF) {
+    switch(c) {
+    case 'd':
+      dp_factor = atoi(optarg);
+      break;
+    case 'c':
+      cuts_per_round = atoi(optarg);
+      break;
+    case 'q':
+      max_q_size = atoi(optarg);
+      break;
+    case 'p':
+      pricing_choice = atoi(optarg);
+      break;
+    case 'R':
+      rand = true;
+      break;
+    case 'S':
+      sparseflag = true;
+      break;
+    case 'X':
+      o_prefs.dump_xy = true;
+      break;
+    case 'g':
+      gridsize = atoi(optarg);
+      break;
+    case 'n':
+      ncount = atoi(optarg);
+      break;
+    case 's':
+      seed = atoi(optarg);
+      break;
+    case 'u':
+      qnearest = atoi(optarg);
+      break;
+    case 'o':
+      tourprefs = atoi(optarg);
+      break;
+    case 't':
+      tourfname = optarg;
+      break;
+    case '?':
+    default:
+      usage(av[0]);
+      return 1;
+    }
+  }
+
+  if(optind < ac)
+    fname = av[optind++];
+  if(optind != ac){
+    usage(av[0]);
+    return 1;
+  }
+
+  if(fname.empty() && ncount == 0){
+    printf("Must specify a problem file or nodecount for random prob\n");
+    usage(av[0]);
+    return 1;
+  }
+
+  if(!fname.empty() && rand){
+    printf("Cannot specify both filename and random problem\n");
+    usage(av[0]);
+    return 1;
+  }
+
+  randprob.nodecount = ncount;
+  randprob.gridsize = gridsize;
+  randprob.seed = seed;
+
+  switch(pricing_choice){
+  case 0:
+    prefs.price_method = CMR::LP::Pricing::Devex;
+    std::cout << "Devex pricing\n";
+    break;
+  case 1:
+    prefs.price_method = CMR::LP::Pricing::SlackSteepest;
+    std::cout << "Steepest edge w slack initial norms\n";
+    break;
+  case 2:
+    prefs.price_method = CMR::LP::Pricing::Steepest;
+    std::cout << "True steepest edge\n";
+    break;
+  default:
+    std::cout << "Pricing method " << pricing_choice << " out of range\n";
+    usage(av[0]);
+    return 1;
+  }
+
+  prefs.dp_threshold = 5 * dp_factor;
+  if(dp_factor >= 0)
+    std::cout << "DP separation will be tried every "
+	 << prefs.dp_threshold << " non-degenerate pivots w no augmentation\n";
+
+  if(cuts_per_round < 1 || max_q_size < 1 || max_q_size < cuts_per_round){
+    std::cerr << "Invalid cuts per round or queue capacity\n";
+    usage(av[0]);
+    return 1;
+  }
+  prefs.max_per_round = cuts_per_round;
+  prefs.q_max_size = max_q_size;
+
+  if(qnearest < 0 || qnearest > 10){
+    std::cerr << "Invalid choice of quad-nearest density\n";
+    usage(av[0]);
+    return 1;
+  }
+
+  switch(tourprefs) {
+  case 3:
+    o_prefs.save_tour = false;
+    o_prefs.save_tour_edges = false;
+    break;
+  case 1:
+    o_prefs.save_tour = false;
+    o_prefs.save_tour_edges = true;
+    break;
+  case 2:
+    o_prefs.save_tour = true;
+    o_prefs.save_tour_edges = true;
+    break;
+  case 0: default:
+    o_prefs.save_tour = true;
+    break;
+  }
+
+  return 0;
+}
+
+static void usage(const std::string &fname){
+  fprintf(stderr, "Usage: %s [-see below-] [prob_file]\n", fname.data());
+  fprintf(stderr, "-------FLAG OPTIONS ------------------------------------\n");
+  fprintf(stderr, "-R    generate random problem\n");
+  fprintf(stderr, "-S    only solve sparse instance with edge set from \n");
+  fprintf(stderr,"       10 Lin-Kernighan tours\n");
+  fprintf(stderr, "-X    dump xy-coords to file, if possible \n");
+  fprintf(stderr, "------ PARAMETER OPTIONS (argument x) ------------------\n");
+  fprintf(stderr, "-c    add at most x cuts per round (default 2)\n");
+  fprintf(stderr, "-d    only call simpleDP sep after 5x rounds of cuts \n");
+  fprintf(stderr, "      with no augmentation. (disabled by default)\n");
+  fprintf(stderr, "-g    gridsize for random problem (100 default)\n");
+  fprintf(stderr, "-n    nodecount for random problem (must be nonzero if\n");
+  fprintf(stderr, "      -R is used\n");
+  fprintf(stderr, "-o    tour file output level \n");
+  fprintf(stderr, "    0 (default) only nodes of best tour\n");
+  fprintf(stderr, "    1 only edges of best tour, node node format\n");
+  fprintf(stderr, "    2 both nodes and edges of best tour\n");
+  fprintf(stderr, "    3 nothing about best tour\n");
+  fprintf(stderr, "-p    set primal pricing protocol to:\n");
+  fprintf(stderr, "    0 (default) devex\n");
+  fprintf(stderr, "    1 steepest edge with slack initial norms\n");
+  fprintf(stderr, "    2 true steepest edge.\n");
+  fprintf(stderr, "-q    keep a queue of at most x blossom cuts to \n");
+  fprintf(stderr, "      be checked before calling the blossom separation \n");
+  fprintf(stderr, "      all over again (default 15)\n");
+  fprintf(stderr, "-s    random seed for random problem and Lin-Kernighan.\n");
+  fprintf(stderr, "      If not set, current time will be used.\n");
+  fprintf(stderr, "-t    load initial tour in filename x.\n");
+  fprintf(stderr, "-u    initial edge set will be union of 10 LK tours plus\n");
+  fprintf(stderr, "      quad x-nearest edges (0 default).\n");
+}
+
+#endif
