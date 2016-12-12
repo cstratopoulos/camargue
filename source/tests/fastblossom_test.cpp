dc7084dee08b5592259dd89d53e040417b9bd174
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-12-12 17:39:21 -0500

    added cuttranslate to tests

--- source/tests/fastblossom_test.cpp
+++ source/tests/fastblossom_test.cpp
@@ -1,6 +1,7 @@
 #include "tests.hpp"
 #include "cc_lpcuts.hpp"
 #include "datagroups.hpp"
+#include "cuts.hpp"
 
 #include <catch.hpp>
 
@@ -16,7 +17,7 @@ using std::vector;
 #ifdef CMR_DO_TESTS
 
 SCENARIO("Primal blossom separation by fast standard heuristics",
-	 "[fast2m]") {
+	 "[fast2m][Sep][CutTranslate]") {
   vector<string> probs{"pr76", "d493", "pr1002"};
   for (string &fname : probs) {
     GIVEN("TSP instance " + fname) {
@@ -30,7 +31,7 @@ SCENARIO("Primal blossom separation by fast standard heuristics",
       CMR::Data::BestGroup b_dat;
       CMR::Data::SupportGroup s_dat;
       std::vector<double> lp_edges;
-      CMR::Cut::LPcutList cutq;
+      CMR::Sep::LPcutList cutq;
 
       WHEN("The tour is good and blossoms exit") {
 	THEN("Primal blossoms are found") {
@@ -43,8 +44,29 @@ SCENARIO("Primal blossom separation by fast standard heuristics",
 			     b_dat.perm);
 	  for (int &i : s_dat.support_elist) i = b_dat.perm[i];
 	
-	  CMR::Cut::FastBlossoms fb_sep(g_dat, b_dat, s_dat, TG, cutq);
+	  CMR::Sep::FastBlossoms fb_sep(g_dat, b_dat, s_dat, TG, cutq);
 	  REQUIRE(fb_sep.find_cuts());
+
+          vector<int> &perm = b_dat.perm;
+
+          CMR::CutTranslate processor(g_dat);
+
+          for (auto cur = cutq.begin(); cur; cur = cur->next) {
+              vector<int> rmatind;
+              vector<double> rmatval;
+              char sense;
+              double rhs;
+              
+              processor.get_sparse_row(*cur, perm, rmatind, rmatval, sense,
+                                       rhs);
+              double tour_act = 0, lp_act = 0;
+              
+              processor.get_activity(tour_act, b_dat.best_tour_edges,
+                                     rmatind, rmatval);
+              processor.get_activity(lp_act, lp_edges, rmatind, rmatval);
+              REQUIRE(tour_act == rhs);
+              REQUIRE(lp_act < rhs);
+          }
 	}
       }
       AND_WHEN("The tour is good but the solution is in the blossom polytope") {
@@ -57,7 +79,7 @@ SCENARIO("Primal blossom separation by fast standard heuristics",
 			     b_dat.perm);
 	  for (int &i : s_dat.support_elist) i = b_dat.perm[i];
 	
-	  CMR::Cut::FastBlossoms fb_sep(g_dat, b_dat, s_dat, TG, cutq);
+	  CMR::Sep::FastBlossoms fb_sep(g_dat, b_dat, s_dat, TG, cutq);
 
 	  REQUIRE_FALSE(fb_sep.find_cuts());
       	}	
@@ -67,7 +89,7 @@ SCENARIO("Primal blossom separation by fast standard heuristics",
 }
 
 SCENARIO("Primal heuristic fast blossom sep in tiny instances",
-	 "[fast2m][tiny]") {
+	 "[fast2m][tiny][Sep][CutTranslate]") {
   vector<string> probs{"blossom6", "comb9"};
   for (string &fname : probs) {
     GIVEN("TSP instance " + fname) {
@@ -81,10 +103,10 @@ SCENARIO("Primal heuristic fast blossom sep in tiny instances",
       CMR::Data::BestGroup b_dat;
       CMR::Data::SupportGroup s_dat;
       std::vector<double> lp_edges;
-      CMR::Cut::LPcutList cutq;
+      CMR::Sep::LPcutList cutq;
 
       WHEN("The tour is good") {
-	THEN("Primal blossoms are found") {
+	THEN("Violated primal blossoms are found") {
 	  REQUIRE_NOTHROW(CMR::Data::make_cut_test(probfile, solfile,
 						  subtourfile, g_dat, b_dat,
 						  lp_edges, s_dat));
@@ -93,8 +115,29 @@ SCENARIO("Primal heuristic fast blossom sep in tiny instances",
 			     b_dat.perm);
 	  for (int &i : s_dat.support_elist) i = b_dat.perm[i];
 	
-	  CMR::Cut::FastBlossoms fb_sep(g_dat, b_dat, s_dat, TG, cutq);
+	  CMR::Sep::FastBlossoms fb_sep(g_dat, b_dat, s_dat, TG, cutq);
 	  REQUIRE(fb_sep.find_cuts());
+
+          vector<int> &perm = b_dat.perm;
+
+          CMR::CutTranslate processor(g_dat);
+
+          for (auto cur = cutq.begin(); cur; cur = cur->next) {
+              vector<int> rmatind;
+              vector<double> rmatval;
+              char sense;
+              double rhs;
+              
+              processor.get_sparse_row(*cur, perm, rmatind, rmatval, sense,
+                                       rhs);
+              double tour_act = 0, lp_act = 0;
+              
+              processor.get_activity(tour_act, b_dat.best_tour_edges,
+                                     rmatind, rmatval);
+              processor.get_activity(lp_act, lp_edges, rmatind, rmatval);
+              REQUIRE(tour_act == rhs);
+              REQUIRE(lp_act < rhs);
+          }
 	}
       }
 
@@ -109,7 +152,7 @@ SCENARIO("Primal heuristic fast blossom sep in tiny instances",
 			     b_dat.perm);
 	  for (int &i : s_dat.support_elist) i = b_dat.perm[i];
 	
-	  CMR::Cut::FastBlossoms fb_sep(g_dat, b_dat, s_dat, TG, cutq);
+	  CMR::Sep::FastBlossoms fb_sep(g_dat, b_dat, s_dat, TG, cutq);
 
 	  REQUIRE_FALSE(fb_sep.find_cuts());
 	}
@@ -117,95 +160,5 @@ SCENARIO("Primal heuristic fast blossom sep in tiny instances",
     }
   }
 }
-
-/*
-SCENARIO("Black box testing of tiny fast blossoms",
-	  "[.fast2m][.tiny]") {
-  CMR::Cut::LPcutList wrap;
-  CMR::Data::GraphGroup g_dat;
-  CMR::Data::BestGroup b_dat;
-  CMR::Data::SupportGroup s_dat;
-  std::vector<double> lp_edges;
-  
-  vector<string> probs{"blossom6", "comb9"};
-  for (string &fname : probs) {
-    GIVEN("TSP instance " + fname) {
-      string
-	probfile = "problems/" + fname + ".tsp",
-	solfile = "test_data/tours/" + fname + ".sol",
-	badsolfile = "test_data/tours/" + fname + ".bad.sol",
-	subtourfile = "test_data/subtour_lp/" + fname + ".sub.x";
-
-      WHEN("The tour is good") {
-	REQUIRE_NOTHROW(CMR::Data::make_cut_test(probfile, solfile,
-						subtourfile,
-						g_dat, b_dat, lp_edges,
-						s_dat));
-
-	CMR::TourGraph TG(b_dat.best_tour_edges,
-			   g_dat.m_graph.edges,
-			   b_dat.perm);
-	for (int &i : s_dat.support_elist) i = b_dat.perm[i];
-	
-	THEN("Odd component blossoms found, all are primal") {
-	  REQUIRE_FALSE(CCtsp_fastblossom(wrap.pass_ptr(), wrap.count_ptr(),
-					  s_dat.G_s.node_count,
-					  s_dat.G_s.edge_count,
-					  &s_dat.support_elist[0],
-					  &s_dat.support_ecap[0]));
-	  REQUIRE(wrap.size() == 2);
-	  wrap.filter_primal(TG);
-	  REQUIRE(wrap.size() == 2);
-	}
-
-	AND_THEN("Grostschel-Holland blossoms found, all are primal") {
-	  REQUIRE_FALSE(CCtsp_ghfastblossom(wrap.pass_ptr(), wrap.count_ptr(),
-					    s_dat.G_s.node_count,
-					    s_dat.G_s.edge_count,
-					    &s_dat.support_elist[0],
-					    &s_dat.support_ecap[0]));
-	  REQUIRE(wrap.size() == 2);
-	  wrap.filter_primal(TG);
-	  REQUIRE(wrap.size() == 2);
-	}
-      }
-
-      AND_WHEN("The tour is bad") {
-	REQUIRE_NOTHROW(CMR::Data::make_cut_test(probfile, badsolfile,
-						subtourfile,
-						g_dat, b_dat, lp_edges,
-						s_dat));
-
-	CMR::TourGraph TG(b_dat.best_tour_edges,
-			   g_dat.m_graph.edges,
-			   b_dat.perm);
-	for (int &i : s_dat.support_elist) i = b_dat.perm[i];
-	
-	THEN("Odd component blossoms found, none are primal") {
-	  REQUIRE_FALSE(CCtsp_fastblossom(wrap.pass_ptr(), wrap.count_ptr(),
-					  s_dat.G_s.node_count,
-					  s_dat.G_s.edge_count,
-					  &s_dat.support_elist[0],
-					  &s_dat.support_ecap[0]));
-	  REQUIRE(wrap.size() == 2);
-	  wrap.filter_primal(TG);
-	  REQUIRE(wrap.size() == 0);
-	}
-
-	AND_THEN("Grostschel-Holland blossoms found, none are primal") {
-	  REQUIRE_FALSE(CCtsp_ghfastblossom(wrap.pass_ptr(), wrap.count_ptr(),
-					    s_dat.G_s.node_count,
-					    s_dat.G_s.edge_count,
-					    &s_dat.support_elist[0],
-					    &s_dat.support_ecap[0]));
-	  REQUIRE(wrap.size() == 2);
-	  wrap.filter_primal(TG);
-	  REQUIRE(wrap.size() == 0);
-	}
-      }
-    }
-  }
-}
-*/
 
 #endif
