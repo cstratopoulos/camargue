b3eebdfb6709574f549d82789ffa02432dad8f9b
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-12-12 15:47:52 -0500

    added tests for core lp

--- source/tests/lp_test.cpp
+++ source/tests/lp_test.cpp
@@ -1,6 +1,7 @@
 #include <catch.hpp>
 
 #include "lp_interface.hpp"
+#include "core_lp.hpp"
 #include "tests.hpp"
 #include "err_util.hpp"
 
@@ -15,8 +16,28 @@ using std::unique_ptr;
 using std::string;
 using std::pair;
 
+#ifdef CMR_DO_TESTS
+
+SCENARIO ("Consructing a Core LP",
+          "[LP][CoreLP]") {
+    vector<string> probs{"dantzig42", "lin318", "pr1002", "pcb3038"};
+
+    for (string& prob : probs) {
+        GIVEN ("The TSP instance " + prob) {
+            WHEN ("A core LP is constructed") {
+                THEN ("Its constructor doesn't throw.") {
+                    CMR::Data::Instance inst("problems/" + prob + ".tsp", 99);
+                    CMR::Data::GraphGroup g_dat(inst);
+                    CMR::Data::BestGroup b_dat(inst, g_dat);
+                    REQUIRE_NOTHROW(CMR::LP::CoreLP core(g_dat, b_dat));
+                }
+            }
+        }
+    }
+}
+
 SCENARIO ("Constructing LP Relaxations",
-          "[LP][Relaxation]") {
+          "[.LP][.Relaxation][valgrind]") {
     WHEN("A relaxation is constructed"){
         THEN("Its constructor doesn't throw"){
             REQUIRE_NOTHROW(CMR::LP::Relaxation rel);            
@@ -34,7 +55,7 @@ SCENARIO ("Constructing LP Relaxations",
     }
 }
 
-SCENARIO ("Failures in constructing LP Relaxations",
+SCENARIO ("Black box testing of failures in constructing LP Relaxations",
           "[.LP][.Relaxaton][!shouldfail][valgrind]") {
     GIVEN ("The raw data structures in an LP Relaxation") {
         WHEN ("A nonzero rval occurs in the constructor") {
@@ -55,3 +76,4 @@ SCENARIO ("Failures in constructing LP Relaxations",
     }
 }
 
+#endif
