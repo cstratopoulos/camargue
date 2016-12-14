94b78c3fc29cdca9bd920a2779112bc814501192
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-12-14 18:18:48 -0500

    implementation, testing, bug fixes

---
+++ source/cliq.cpp
@@ -0,0 +1,151 @@
+#include "cliq.hpp"
+#include "err_util.hpp"
+
+#include <algorithm>
+#include <iostream>
+#include <stdexcept>
+
+using std::cout;
+using std::cerr;
+
+using std::unordered_map;
+using std::vector;
+
+using std::runtime_error;
+using std::logic_error;
+using std::exception;
+
+using lpclique = CCtsp_lpclique;
+using lpcut_in = CCtsp_lpcut_in;
+
+namespace CMR {
+namespace Sep {
+
+Clique::Clique(const lpclique &cc_clq,
+               const vector<int> &saved_tour, const vector<int> &saved_perm,
+               const vector<int> &current_tour)
+try
+{
+    for (int i = 0; i < cc_clq.segcount; ++i) {
+        segment seg(cc_clq.nodes[i].lo, cc_clq.nodes[i].hi);
+        bool range_agrees = true;
+
+        for (int k = seg.start; k <= seg.end; ++k) {
+            if (saved_tour[k] != current_tour[k]) {
+                range_agrees = false;
+                break;
+            }
+        }
+
+        if (range_agrees) {
+            seglist.push_back(seg);
+            continue;
+        }
+
+        vector<int> seg_nodes;
+        seg_nodes.reserve(seg.size());
+        
+        for (int k = seg.start; k <= seg.end; ++k)
+            seg_nodes.push_back(saved_perm[current_tour[k]]);
+
+        std::sort(seg_nodes.begin(), seg_nodes.end());
+
+        int k = 0;
+
+        while (k < seg_nodes.size()) {
+            int low = seg_nodes[k];
+            
+            while ((k < (seg_nodes.size() - 1)) &&
+                   (seg_nodes[k + 1] == (seg_nodes[k] + 1)))
+                ++k;
+            
+            seglist.push_back(segment(low, seg_nodes[k++]));
+        }
+    }
+
+    std::sort(seglist.begin(), seglist.end(), std::greater<segment>());
+} catch (const exception &e) {
+    cerr << e.what() << "\n";
+    throw runtime_error("Clique CCtsp_lpclique constructor failed.");
+}
+
+Clique::Clique(std::vector<int> &nodes, const std::vector<int> &perm)
+try
+{
+    for (int i = 0; i < nodes.size(); ++i)
+        nodes[i] = perm[nodes[i]];
+
+    std::sort(nodes.begin(), nodes.end());    
+
+    int i = 0;
+
+    while (i < nodes.size()) {
+        int low = nodes[i];
+
+        while ((i < (nodes.size() - 1)) && (nodes[i + 1] == (nodes[i] + 1)))
+            ++i;
+
+        seglist.push_back(segment(low, nodes[i++]));        
+    }
+
+    std::sort(seglist.begin(), seglist.end(), std::greater<segment>());
+} catch (const exception &e) {
+    cerr << e.what() << "\n";
+    throw runtime_error("Clique nodelist constructor failed.");
+}
+
+vector<int> Clique::node_list(const vector<int> &saved_tour) const
+{
+    vector<int> result;
+
+    for (const segment &seg : seglist)
+        for (int k = seg.start; k <= seg.end; ++k) 
+            result.push_back(saved_tour[k]);
+
+    return result;
+}
+
+CliqueBank::CliqueBank(const vector<int> &tour, const vector<int> &perm)
+try : saved_tour(tour), saved_perm(perm) {} catch (const exception &e) {
+    throw runtime_error("CliqueBank constructor failed.");
+}
+
+Clique::Ptr& CliqueBank::add_clique(const Clique &clq)
+{
+    if (bank.count(clq) == 0)
+        bank[clq] = std::make_shared<Clique>(clq);
+
+    return bank[clq];
+}
+
+Clique::Ptr& CliqueBank::add_clique(const CCtsp_lpclique &cc_clq,
+                                    const vector<int> &tour)
+{
+    return add_clique(Clique(cc_clq, saved_tour, saved_perm, tour));
+}
+
+Clique::Ptr& CliqueBank::add_clique(vector<int> &nodes)
+{
+    return add_clique(Clique(nodes, saved_perm));
+}
+
+void CliqueBank::del_clique(Clique::Ptr &clq_ptr)
+{
+    if (!clq_ptr)
+        return;
+
+    Clique &clq = *clq_ptr;
+
+    CliqueBank::Itr find_it = bank.find(clq);
+
+    if (find_it == bank.end())
+        return;
+
+    clq_ptr.reset();
+
+    if (find_it->second.use_count() <= 1)
+        bank.erase(find_it);
+}
+
+}
+}
--- source/tests/cliq_test.cpp
+++ source/tests/cliq_test.cpp
@@ -2,6 +2,7 @@
 #include "cliq.hpp"
 #include "util.hpp"
 
+#include <algorithm>
 #include <vector>
 #include <string>
 #include <iostream>
@@ -16,8 +17,96 @@ using std::cout;
 
 #ifdef CMR_DO_TESTS
 
-SCENARIO ("Constructing cliques") {
-    CMR::Sep::Clique clq;
+SCENARIO ("Abstract testing of tiny printed cliques",
+          "[Clique][Sep][tiny]") {
+    vector<int> test_sizes{10, 12, 14, 16};
+
+    for (int sz : test_sizes) {
+        GIVEN ("A tour on " + std::to_string(sz) + " nodes.") {
+            vector<int> tour(sz);
+            vector<int> perm(sz);
+
+            for (int i = 0; i < sz; ++i)
+                tour[i] = i;
+
+            std::random_shuffle(tour.begin(), tour.end());
+
+            for (int i = 0; i < sz; ++i)
+                perm[tour[i]] = i;
+
+            cout << "Tour:\n";
+            for (int i : tour) cout << i << ", ";
+            cout << "\n";
+
+            cout << "Perm:\n";
+            for (int i : perm) cout << i << ", ";
+            cout << "\n";
+            WHEN ("We grab contiguous sections") {
+                vector<int> cts1(tour.begin(), tour.begin() + (sz / 4));
+                vector<int> cts2(tour.begin() + (sz / 3),
+                                 tour.begin() + ((2 * sz)/3));
+                vector<int> cts3(tour.end() - (sz/5), tour.end());
+
+                vector<vector<int>> cts_nodes{cts1, cts2, cts3};
+
+                THEN ("We get a single-segment clique w the expected size") {
+                    for (auto &vec : cts_nodes) {
+                        vector<int> vec_copy = vec;
+                        cout << "nodelist: ";
+                        for(int i : vec) cout << i << ", ";
+                        cout << "\n";
+                        CMR::Sep::Clique clq  = CMR::Sep::Clique(vec, perm);
+                        const CMR::Sep::segment seg = clq.seg_list().front();
+                        cout << "Represented as: " << seg.start << ", "
+                             << seg.end << "\n";
+                        
+                        CHECK(clq.seg_count() == 1);
+                        CHECK(seg.size() == vec.size());
+
+                        vector<int> from_clq = clq.node_list(tour);
+
+                        CHECK(from_clq == vec_copy);
+                    }
+                }                
+            }
+
+            WHEN ("We grab disjoint segments") {
+                vector<int> one_node{tour.front()};
+                vector<int> edge{tour[2], tour[(2 * sz) / 3]};
+                vector<int> nodes_and_seg{tour[0]};
+                nodes_and_seg.push_back(tour[2]);
+                for (auto it = tour.begin() + ((3 * sz) / 4); it != tour.end();
+                     ++it)
+                    nodes_and_seg.push_back(*it);
+
+                using VecInt = std::pair<std::vector<int>, int>;
+
+                vector<VecInt> vec_expect{VecInt(one_node, 1),
+                                          VecInt(edge, 2),
+                                          VecInt(nodes_and_seg, 3)};
+
+                THEN ("We get cliques with the expected size") {
+                    for (auto &vp : vec_expect) {
+                        vector<int> vec_copy = vp.first;
+                        int expect_sz = vp.second;
+
+                        cout << "nodelist: ";
+                        for(int i : vp.first) cout << i << ", "; cout << "\n";
+
+                        CMR::Sep::Clique clq = CMR::Sep::Clique(vp.first, perm);
+                        CHECK(clq.seg_count() == expect_sz);
+
+                        vector<int> from_clq = clq.node_list(tour);
+
+                        std::sort(from_clq.begin(), from_clq.end());
+                        std::sort(vec_copy.begin(), vec_copy.end());
+
+                        CHECK(from_clq == vec_copy);
+                    }
+                }
+            }
+        }
+    }
 }
 
 #endif //CMR_DO_TESTS
