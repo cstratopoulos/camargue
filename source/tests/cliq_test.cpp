3d745f40a2a04c015f773303fe46fdd2e8091090
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-12-14 19:15:20 -0500

    tests of cc clique constructor

--- source/tests/cliq_test.cpp
+++ source/tests/cliq_test.cpp
@@ -1,5 +1,7 @@
 #include "tests.hpp"
 #include "util.hpp"
+#include "core_lp.hpp"
+#include "datagroups.hpp"
 #include "cliq.hpp"
 
 
@@ -18,8 +20,77 @@ using std::cout;
 
 #ifdef CMR_DO_TESTS
 
+SCENARIO ("Generating cliques from Concorde cliques",
+          "[Clique][Sep]") {
+    vector<string> probs{"pr76", "lin318", "d493", "pr1002"};
+    for (string &fname : probs) {
+        GIVEN("TSP instance " + fname) {
+            WHEN("We find primal blossoms") {p
+                THEN("We can represent them with CMR cliques") {
+                    string
+                    probfile = "problems/" + fname + ".tsp",
+                    solfile = "test_data/tours/" + fname + ".sol",
+                    blossomfile = "test_data/blossom_lp/" + fname + ".2m.x",
+                    subtourfile = "test_data/subtour_lp/" + fname + ".sub.x";
+
+                    CMR::Data::GraphGroup g_dat;
+                    CMR::Data::BestGroup b_dat;
+                    CMR::Data::SupportGroup s_dat;
+                    std::vector<double> lp_edges;
+                    CMR::Sep::LPcutList cutq;
+                    REQUIRE_NOTHROW(CMR::Data::make_cut_test(probfile, solfile,
+                                                             subtourfile,
+                                                             g_dat, b_dat,
+                                                             lp_edges, s_dat));
+
+	
+                    CMR::TourGraph TG(b_dat.best_tour_edges,
+                                      g_dat.m_graph.edges,
+                                      b_dat.perm);
+                    for (int &i : s_dat.support_elist) i = b_dat.perm[i];
+	
+                    CMR::Sep::FastBlossoms fb_sep(s_dat.support_elist,
+                                                  s_dat.support_ecap, TG, cutq);
+                    REQUIRE(fb_sep.find_cuts());
+
+                    vector<int> &perm = b_dat.perm;
+                    vector<int> &tour = b_dat.best_tour_nodes;
+
+                    for (auto cur = cutq.begin(); cur; cur = cur->next) {
+                        CCtsp_lpcut_in &cc_cut = *cur;
+                        for (int i = 0; i < cc_cut.cliquecount; ++i) {
+                            CCtsp_lpclique &cc_clq = cc_cut.cliques[i];
+
+                            CMR::Sep::Clique clq(cc_clq, tour, perm, tour);
+
+                            vector<int> cmr_nodes = clq.node_list(tour);
+                            int *ar;
+                            int count;
+
+                            REQUIRE_FALSE(CCtsp_clique_to_array(&cc_clq, &ar,
+                                                                &count));
+
+                            vector<int> cc_nodes;
+
+                            for (int i = 0; i < count; ++i)
+                                cc_nodes.push_back(tour[ar[i]]);
+
+                            std::sort(cmr_nodes.begin(), cmr_nodes.end());
+                            std::sort(cc_nodes.begin(), cc_nodes.end());
+
+                            REQUIRE(cmr_nodes == cc_nodes);
+                  
+                            free(ar);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}                
+
 SCENARIO ("Abstract testing of tiny printed cliques",
-          "[Clique][Sep][tiny]") {
+          "[.Clique][.Sep][tiny]") {
     vector<int> test_sizes{10, 12, 14, 16};
 
     for (int sz : test_sizes) {
