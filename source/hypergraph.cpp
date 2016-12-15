edae5f038303f0e721edc87256a3e98468fd9d96
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-12-15 18:09:37 -0500

    implementing constructors

--- source/hypergraph.cpp
+++ source/hypergraph.cpp
@@ -5,6 +5,7 @@
 #include <utility>
 
 using std::vector;
+using std::pair;
 
 using std::cout;
 using std::cerr;
@@ -37,6 +38,38 @@ HyperGraph::~HyperGraph()
 {
     for (Clique::Ptr &ref : cliques)
         source_bank.del_clique(ref);
+}
+
+DominoCut::DominoCut(CliqueBank &bank, CMR::dominoparity &dp_cut,
+                     const vector<int> &tour) try :
+    source_bank(bank)
+{
+    for (int &i : dp_cut.degree_nodes)
+        i = tour[i];
+
+    for (auto &ends : dp_cut.nonneg_edges) {
+        ends.first = tour[ends.first];
+        ends.second = tour[ends.second];
+    }
+
+    nonneg_edges = dp_cut.nonneg_edges;
+    
+    for (CMR::SimpleTooth &T : dp_cut.used_teeth) {
+        teeth.push_back(pair<int, Clique::Ptr>(tour[T.root],
+                                               bank.add_clique(T.body_start,
+                                                               T.body_end,
+                                                               tour)));
+    }
+} catch (const exception &e) {
+    cerr << e.what() << "\n";
+    throw runtime_error("Problem in DominoCut constructor.");
+}
+
+DominoCut::~DominoCut()
+{
+    source_bank.del_clique(handle);
+    for(auto &tooth : teeth)
+        source_bank.del_clique(tooth.second);
 }
 
 
