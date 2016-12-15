811488e245b146534456f59d1f0e17c861325703
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-12-15 18:53:55 -0500

    preliminary test of pure cut loop

---
+++ source/tests/purecut_test.cpp
@@ -0,0 +1,108 @@
+#include "tests.hpp"
+#include "util.hpp"
+#include "datagroups.hpp"
+#include "core_lp.hpp"
+#include "cutcontrol.hpp"
+#include "cliq.hpp"
+#include "timer.hpp"
+
+
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iostream>
+
+#include <cstdlib>
+
+#include <catch.hpp>
+
+using std::min;
+using std::max;
+using std::array;
+using std::vector;
+
+using std::string;
+using std::cout;
+
+#ifdef CMR_DO_TESTS
+
+SCENARIO ("Testing a prototype Purecut loop",
+          "[PureCut][Solver]") {
+    vector<string> probs{
+//        "dantzig42", "gr48", "st70", "pr76", "rd100", "lin105"
+        "a280", "lin318", "d493", "att532", "p654", "u724"
+        //"pr1002", "d2103", "pr2392"
+    };
+
+    for (string &prob : probs) {
+        GIVEN (prob) {
+            THEN ("We can run a pure cut solve loop.") {
+                CMR::Data::Instance inst("problems/" + prob + ".tsp", 0);
+                CMR::Data::GraphGroup g_dat(inst);
+                CMR::Data::BestGroup b_dat(inst, g_dat, prob + ".sol");
+                CMR::Data::KarpPartition kpart(b_dat.perm.size(),
+                                               inst.ptr(), inst.seed());
+
+                CMR::LP::CoreLP core(g_dat, b_dat);
+                CMR::TourGraph TG(b_dat.best_tour_edges,
+                                  g_dat.m_graph.edges,
+                                  b_dat.perm);
+
+                CMR::LP::PivType piv = CMR::LP::PivType::Tour;
+                bool found_cuts = true;
+                int round = 0;
+
+                CMR::Timer timer(prob);
+                timer.start();
+
+                while (true) {
+                    ++round;
+                    piv = core.primal_pivot();
+
+                    if (piv == CMR::LP::PivType::FathomedTour) {
+                        cout << "\t ROUND " << round << "\n";
+                        cout << "\t Tour fathomed optimal!\n";
+                        break;
+                    }
+                    if (piv == CMR::LP::PivType::Tour) {
+                        cout << "\t ROUND " << round << "\n";
+                        cout << "\t Augmented to tour of length: "
+                             << core.get_objval() << "\n";
+                        piv = CMR::LP::PivType::Frac;
+                        continue;
+                    }
+                    
+                    CMR::Data::SupportGroup &s_dat = core.supp_data;
+
+                    CMR::Sep::CutControl control(g_dat, b_dat, s_dat, kpart);
+
+                    if (!control.find_cuts(TG)) {
+                        cout << "\tROUND " << round << "\n";
+                        cout << "\tNo cuts found, objval: "
+                             << core.get_objval() << ", stat: "
+                             << CMR::LP::piv_string(piv) << "\n";
+                        break;
+                    }
+                    
+                    core.pivot_back();
+
+                    core.add_cuts(control.seg_q);
+                    core.add_cuts(control.fast2m_q);
+                    core.add_cuts(control.blkcomb_q);
+                    core.add_cuts(control.connect_q);
+
+                    core.add_cuts(control.dp_q);                    
+                }
+                timer.stop();
+                cout << "\t" << (core.num_rows() - g_dat.m_graph.node_count)
+                     << " cuts, " << core.num_cols() << " cols in LP.\n";
+                cout << "\t Optimal obj value: " << core.opt() << "\n";
+                timer.report(false);
+                cout << "\n\n";
+            }
+        }
+    }
+}
+
+
+#endif //CMR_DO_TESTS
