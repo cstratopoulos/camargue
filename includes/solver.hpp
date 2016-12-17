c30174d67ed96f9eff523d197fcbf8e4a03f5611
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-12-17 12:48:02 -0500

    added members/cutting_loop

--- includes/solver.hpp
+++ includes/solver.hpp
@@ -2,6 +2,8 @@
 #define CMR_SOLVER_H
 
 #include "datagroups.hpp"
+#include "core_lp.hpp"
+#include "cutcontrol.hpp"
 #include "util.hpp"
 
 #include <string>
@@ -22,10 +24,19 @@ public:
     Solver(const int seed, const int node_count, const int gridsize,
            const CMR::OutPrefs outprefs);
 
+    CMR::LP::PivType cutting_loop();
+
+    
+
 private:
+    void report_piv(CMR::LP::PivType piv, int round);
+    
     CMR::Data::Instance tsp_instance;
+    CMR::Data::KarpPartition karp_part;
     CMR::Data::GraphGroup graph_data;
     CMR::Data::BestGroup best_data;
+
+    CMR::LP::CoreLP core_lp;
 
     CMR::OutPrefs output_prefs;
 };
