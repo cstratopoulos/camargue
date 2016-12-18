3ef36cf2d0ac26f1e577592178e89594299362f3
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-12-18 09:43:56 -0500

    documentation

--- includes/hypergraph.hpp
+++ includes/hypergraph.hpp
@@ -11,8 +11,14 @@
 namespace CMR {
 namespace Sep {
 
+/** Class for external representation of cuts added to the lp relaxation. */
 class HyperGraph {
 public:
+    /** Construct a HyperGraph from a Concorde cut.
+     * The cut corresponding to \p cc_lpcut will be represented using 
+     * Clique pointers from \p bank, assuming that the cut was found with
+     * \p tour as the resident best tour.
+     */
     HyperGraph(CMR::Sep::CliqueBank &bank,
                const CCtsp_lpcut_in &cc_lpcut,
                const std::vector<int> &tour);
@@ -47,15 +53,23 @@ private:
     CMR::Sep::CliqueBank &source_bank;
 };
 
+/** Class for managing a list of cuts in an lp relaxation. */
 class ExternalCuts {
+    /** Construct ExternalCuts with a reference tour and perm. */
     ExternalCuts(const std::vector<int> &tour, const std::vector<int> &perm);
 
+    /** Add a cut in Concorde format. */
     void add_cut(const CCtsp_lpcut_in &cc_lpcut,
                  const std::vector<int> &current_tour);
 
+    /** Add a simple DP cut. */
     void add_cut(CMR::dominoparity &dp_cut,
                  const std::vector<int> &current_tour);
 
+    /** Delete the cuts indicated by \p delset. 
+     * If `delset[i] == -1` then `cuts[i]` will be deleted, else `cuts[i]` 
+     * will become `cuts[delset[i]]`
+     */
     void del_cuts(const std::vector<int> &delset,
                   int new_num_rows);
 
