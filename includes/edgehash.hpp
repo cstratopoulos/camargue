2097ef101ad61c4f311a0c88f0c80bedd3eab8fd
Christos Stratopoulos <c.stratopoulos@gmail.com>
2016-12-23 12:01:02 -0500

    implementation of methods

--- includes/edgehash.hpp
+++ includes/edgehash.hpp
@@ -1,6 +1,7 @@
 #ifndef CMR_EDGEHASH_H
 #define CMR_EDGEHASH_H
 
+#include "Graph.hpp"
 #include "util.hpp"
 
 extern "C" {
@@ -9,19 +10,33 @@ extern "C" {
 
 #include <iostream>
 #include <memory>
-#include <stdexcept>
 #include <utility>
+#include <vector>
 
 namespace CMR {
 namespace util {
 
+/** Mapping node pairs to edges. */
 class EdgeHash {
 public:
-    EdgeHash(int size);
+    EdgeHash(int size); /**< Construct an EdgeHash for approx size elements. */
+
+    void add(int end1, int end2, int val); /**< Add a pair. */
+    void set(int end1, int end2, int val); /**< Set val for existing pair. */
+    void erase(int end1, int end2); /**< Delete a pair. */
+
+    std::vector<Edge> get_all(); /**< Get a vector of all the edges. */
+
+    void clear(); /**< Clear all the edges. */
+
+    int get_val(int end1, int end2); /**< Get the val for an edge. */
+    
 
 private:
     static constexpr int factor = 1.5;
     c_struct_ptr<CCutil_edgehash> hash_handle;
+
+    CCutil_edgehash *ptr() { return hash_handle.get(); };
 };
 
 
--- source/edgehash.cpp
+++ source/edgehash.cpp
@@ -1,5 +1,13 @@
 #include "edgehash.hpp"
 
+#include <stdexcept>
+
+using std::exception;
+using std::runtime_error;
+using std::logic_error;
+
+using std::vector;
+
 namespace CMR {
 namespace util {
 
@@ -14,6 +22,57 @@ EdgeHash::EdgeHash(int size) try :
     std::cerr << e.what() << "\n";
     throw std::runtime_error("EdgeHash constructor failed.");
 }
+
+void EdgeHash::add(int end1, int end2, int val)
+{
+    if (CCutil_edgehash_add(ptr(), end1, end2, val))
+        throw runtime_error("CCutil_edgehash_add failed.");
+}
+
+void EdgeHash::set(int end1, int end2, int val)
+{
+    if (CCutil_edgehash_set(ptr(), end1, end2, val))
+        throw runtime_error("CCutil_edgehash_set failed.");
+}
+
+void EdgeHash::erase(int end1, int end2)
+{
+    if (CCutil_edgehash_del(ptr(), end1, end2))
+        throw runtime_error("CCutil_edgehash_del failed.");
+}
+
+vector<Edge> EdgeHash::get_all()
+{
+    int ecount = 0;
+    int *elist;
+    int *elen;
+
+    if (CCutil_edgehash_getall(ptr(), &ecount, &elist, &elen))
+        throw runtime_error("CCutil_edgehash_getall failed.");
+
+    util::c_array_ptr elist_owner(elist);
+    util::c_array_ptr elen_owner(elen);
+
+    vector<Edge> result(ecount);
+
+    for (int i = 0; i < ecount; ++i)
+        result[i] = Edge(elist[2 * i], elist[(2 * i) +1], elen[i]);
+
+    return result;
+}
+
+void EdgeHash::clear() { CCutil_edgehash_delall(ptr()); }
+
+int EdgeHash::get_val(int end1, int end2)
+{
+    int val = 0;
+
+    if (CCutil_edgehash_find(ptr(), end1, end2, &val) == -1)
+        return -1;
+
+    return val;
+}
+
 
 }
 }
