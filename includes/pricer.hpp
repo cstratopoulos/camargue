17e65d5d1771a899fdad72298449c805b94ba630
Christos Stratopoulos <c.stratopoulos@gmail.com>
2017-01-05 14:43:00 -0500

    adding and implementing methods

--- includes/pricer.hpp
+++ includes/pricer.hpp
@@ -4,7 +4,11 @@
 #include "lp_interface.hpp"
 #include "datagroups.hpp"
 #include "hypergraph.hpp"
-#include "util.hpp"
+#include "price_util.hpp"
+
+#include <queue>
+#include <unordered_map>
+#include <vector>
 
 namespace CMR {
 
@@ -16,18 +20,49 @@ constexpr int nearest_factor = 50;
 /** Class for pricing edges not in the CoreLP. */
 class Pricer {
 public:
+    /** Construct a Pricer.
+     * @param[in] _relax the Relaxation for grabbing dual values
+     * @param[in] _inst the TSP instance for generating edges
+     * @param[in] _ext_cuts the HyperGraph representation of the cuts in 
+     * \p _relax.
+     */
     Pricer(const LP::Relaxation &_relax, const Data::Instance &_inst,
            const Sep::ExternalCuts &_ext_cuts);
 
+    /** Generate edges to add to the CoreLP.
+     * @param[in] piv_stat the PivType from the solution when edge generation 
+     * is called. Used to determine which pricing protocol will be used.
+     * @returns a ScanStat indicating the outcome of the pricing scan.
+     */
+    ScanStat add_edges(LP::PivType piv_stat);
+
 private:
+    /// Scan the 50-nearest edges.
+    /// @returns true iff negative reduced cost edges were found.
+    ScanStat partial_scan();
+
+    /// Scan the full edge set.
+    /// @returns true iff negative reduced cost edges were found.
+    ScanStat full_scan(); 
+    
     const LP::Relaxation &relax;
     const Data::Instance &inst;
     const Sep::ExternalCuts &ext_cuts;
+
+    std::vector<double> node_pi;
+    std::vector<double> node_pi_est;
+    
+    std::vector<double> cut_pi;
+
+    std::unordered_map<Sep::Clique, double> clique_pi;
     
     int last_ind;
 
     util::c_array_ptr nearest_elist;
     int nearest_ecount;
+
+
+    std::vector<edge> edge_q;
 
 };
 
--- source/pricer.cpp
+++ source/pricer.cpp
@@ -1,21 +1,35 @@
 #include "pricer.hpp"
+#include "err_util.hpp"
 
 extern "C" {
 #include <concorde/INCLUDE/edgegen.h>
 }
 
+#include <iostream>
 #include <stdexcept>
+#include <utility>
+
+using std::vector;
+
+using std::cout;
+using std::cerr;
 
 using std::exception;
 using std::runtime_error;
 using std::logic_error;
 
 namespace CMR {
+
+using LP::PivType;
+
 namespace Price {
 
 Pricer::Pricer(const LP::Relaxation &_relax, const Data::Instance &_inst,
                const Sep::ExternalCuts &_ext_cuts) try :
-    relax(_relax), inst(_inst), ext_cuts(_ext_cuts), last_ind(0)
+    relax(_relax), inst(_inst), ext_cuts(_ext_cuts),
+    node_pi(vector<double>(inst.node_count())),
+    cut_pi(vector<double>(relax.num_rows() - inst.node_count())),
+    last_ind(0)
 {
     int ncount = inst.node_count();
     CCedgegengroup plan;
@@ -43,6 +57,69 @@ Pricer::Pricer(const LP::Relaxation &_relax, const Data::Instance &_inst,
     
 } catch (const exception &e) {
     throw runtime_error("Pricer constructor failed.");
+}
+
+ScanStat Pricer::add_edges(PivType piv_stat)
+{
+    runtime_error err("Problem in Pricer::add_edges");
+    ScanStat result = ScanStat::Partial;
+
+    const vector<Sep::HyperGraph> &cutlist = ext_cuts.get_cuts();
+    const Sep::CliqueBank &clq_bank = ext_cuts.get_cbank();
+    const vector<int> &def_tour = clq_bank.ref_tour();
+    
+    try {
+        relax.get_pi(node_pi, 0, inst.node_count() - 1);
+        node_pi_est = node_pi;
+        
+        relax.get_pi(cut_pi, inst.node_count(), relax.num_rows() - 1);
+        
+        clique_pi.clear();
+        clique_pi.reserve(clq_bank.size());
+    } CMR_CATCH_PRINT_THROW("clearing/getting/updating pi values", err);
+
+    for (int i = 0; i < cutlist.size(); ++i) {
+        const Sep::HyperGraph &H = cutlist[i];
+        if (H.cut_type() == Sep::HyperGraph::Type::Domino)
+            continue;
+
+        double pival = cut_pi[i];
+
+        for (const Sep::Clique::Ptr &clq_ref : H.get_cliques()) {
+            if (clique_pi.count(*clq_ref) == 0)
+                clique_pi[*clq_ref] = 0.0;
+            
+            clique_pi[*clq_ref] += pival;
+        }
+    }
+
+    for (const std::pair<Sep::Clique, double> &kv : clique_pi) {
+        const Sep::Clique &clq = kv.first;
+        double pival = kv.second;
+
+        if (pival > 0.0) {
+            for (const Segment &seg : clq.seg_list()) {
+                for (int k = seg.start; k <= seg.end; ++k) {
+                    int node = def_tour[k];
+                    
+                    node_pi[node] += pival;
+                    node_pi_est[node] += pival;
+                }
+            }            
+        } else if (pival < 0.0) {
+            for (const Segment &seg : clq.seg_list()) {
+                for (int k = seg.start; k <= seg.end; ++k) {
+                    int node = def_tour[k];
+                    
+                    node_pi[node] += pival;
+                }
+            }
+        }
+    }
+
+    
+
+    return result;    
 }
 
 }
