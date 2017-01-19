4660050f25d71922de996f46ab71d5a80b3c41e3
christos2 <c.stratopoulos@gmail.com>
2017-01-19 11:45:28 -0500

    fresh tests to compare approaches

--- source/tests/branch_test.cpp
+++ source/tests/branch_test.cpp
@@ -1,7 +1,9 @@
 #include "config.hpp"
 
-#ifdef CMR_DO_TESTSfdfds
+#ifdef CMR_DO_TESTS
 
+#include "lp_interface.hpp"
+#include "branch_util.hpp"
 #include "solver.hpp"
 #include "util.hpp"
 #include "timer.hpp"
@@ -29,22 +31,12 @@ using std::string;
 using std::to_string;
 using std::cout;
 
-constexpr double EpsZero = CMR::Epsilon::Zero;
-using ScorePair = std::pair<int, double>;
-
-static bool sp_sort_greater(const ScorePair &p, const ScorePair &r)
-{
-    return p.second > r .second;
-}
-
-static bool is_integral(double d) { return d >= EpsZero && d <= 1 - EpsZero; }
-
-
-
 SCENARIO ("Computing branching edges",
-          "[Solver][Branch][ABC]") {
+          "[ABC]") {
     vector<string> probs{
-        "d493", "pr1002", "d2103", "pr2392"
+        "pr152",
+        "a280",
+        "lin318",
     };
 
     for (string &prob : probs) {
@@ -54,163 +46,81 @@ SCENARIO ("Computing branching edges",
                 CMR::Solver solver("problems/" + prob + ".tsp",
                                    //prob + ".sol",
                                    99, prefs);
-
-                CMR::LP::PivType piv;
-
-                REQUIRE_NOTHROW(piv = solver.cutting_loop());
+                CMR::LP::PivType piv = solver.cutting_loop(false);
 
                 if (piv == CMR::LP::PivType::Frac) {
-                    vector<int> frac_colstat;
-                    vector<int> frac_rowstat;
-                    vector<double> frac_x;
+                    CMR::LP::CoreLP &core =
+                    const_cast<CMR::LP::CoreLP &>(solver.get_core_lp());
+                    CMR::LP::Relaxation &rel = core;
 
-                    CMR::LP::Relaxation &rel = solver.relax();
+                    vector<int> colstat;
+                    vector<int> rowstat;
+                    vector<double> x = rel.lp_vec();
+                    vector<int> md_indices;
                     
-                    const CMR::LP::TourBasis &tour_base = solver.tour_basis();
+                    rel.get_base(colstat, rowstat);
 
-                    vector<double> tour_edges = tour_base.best_tour_edges;
-                    vector<int> tour_colstat = tour_base.colstat;
-                    vector<int> tour_rowstat = tour_base.rowstat;
+                    for (int i = 0; i < colstat.size(); ++i)
+                        if (colstat[i] == CMR::LP::BStat::Basic &&
+                            !CMR::util::var_integral(x[i]))
+                            md_indices.push_back(i);
+
+                    REQUIRE_FALSE(md_indices.empty());
+
+                    vector<double> downest;
+                    vector<double> upest;
+
+                    REQUIRE_NOTHROW(rel.get_penalties(md_indices, downest,
+                                                      upest));
                     
-                    REQUIRE_NOTHROW(rel.get_base(frac_colstat, frac_rowstat));
-                    REQUIRE_NOTHROW(rel.get_x(frac_x));
+                    vector<CMR::ABC::ScorePair> md_cands;
 
-                    REQUIRE(frac_colstat.size() > 0);
-
-                    vector<int> cand_inds;
-                    vector<double> downratio;
-                    vector<double> upratio;
-
-                    for (int i = 0; i < frac_x.size(); ++i)
-                        if(is_integral(frac_x[i]) &&
-                            frac_colstat[i] == CPX_BASIC)
-                            cand_inds.push_back(i);
-
-                    REQUIRE(cand_inds.size() > 0);
-                    REQUIRE_NOTHROW(rel.get_penalties(cand_inds, downratio,
-                                                      upratio));
+                    REQUIRE_NOTHROW(md_cands =
+                                    CMR::ABC::ranked_cands(md_indices, downest,
+                                                           upest, 10, 5));
+                    const vector<double> &tour_edges =
+                    solver.tour_basis().best_tour_edges;
                     
-                    vector<ScorePair> db_scores;
+                    double tourval = solver.best_info().min_tour_value;
 
-                    for (int i = 0; i < cand_inds.size(); ++i) {
-                        ScorePair p(cand_inds[i],
-                                    CMR::LP::branch_score(10, downratio[i],
-                                                          upratio[i]));
-                        db_scores.push_back(p);
+                    vector<int> sb1inds;
+
+                    cout << "\t" << md_cands.size() << " driebek cands\n";
+                    
+                    for (auto &sp : md_cands) {
+                        int ind = sp.first;
+                        sb1inds.push_back(ind);
+                        cout << "\tEdge " << ind << "\n\t\ttour: "
+                             << tour_edges[ind] << "\t"
+                             << "lp: " << x[ind] << "\t"
+                             << "Est: " << sp.second << "\n";
                     }
+                    cout << "\n";
 
-                    std::sort(db_scores.begin(), db_scores.end(),
-                              sp_sort_greater);
+                    const vector<int> &tourcol = solver.tour_basis().colstat;
+                    const vector<int> &tourrow = solver.tour_basis().rowstat;
 
-                    db_scores.resize(5);
-                    for (ScorePair &p : db_scores)
-                        cout << "Var " << p.first << " has score "
-                             << p.second << "\n";
-
-                    cand_inds.clear();
-
-                    for (ScorePair &p : db_scores)
-                        cand_inds.push_back(p.first);
-
-                    vector<double> downobj;
-                    vector<double> upobj;
-
-                    const CMR::Data::BestGroup &b_dat = solver.best_info();
-
-                    double tourval = b_dat.min_tour_value;
-
-                    cout << "Doing 100 itlim primal sb with 5 cands...";
-                    double sb1 = CMR::util::zeit();
                     REQUIRE_NOTHROW(rel.primal_strong_branch(tour_edges,
-                                                             tour_colstat,
-                                                             tour_rowstat,
-                                                             cand_inds,
-                                                             downobj,
-                                                             upobj, 100,
+                                                             tourcol, tourrow,
+                                                             sb1inds, downest,
+                                                             upest, 100,
                                                              tourval));
-                    cout << "Done in " << (CMR::util::zeit() - sb1) << "s\n";
+                    vector<CMR::ABC::ScorePair> sb1cands;
 
-                    vector<double> after_x;
-                    
-                    REQUIRE_NOTHROW(after_x = rel.lp_vec());
-                    REQUIRE(after_x == tour_edges);
-                    
+                    REQUIRE_NOTHROW(sb1cands =
+                                    CMR::ABC::ranked_cands(sb1inds, downest,
+                                                           upest, 100, 2));
 
-                    vector<ScorePair> sb1_scores;
-
-                    for (int i = 0; i < cand_inds.size(); ++i) {
-                        ScorePair p(cand_inds[i],
-                                    CMR::LP::branch_score(100, downobj[i],
-                                                          upobj[i]));
-                        sb1_scores.push_back(p);
+                    cout << "\t" << sb1cands.size() << " sb1 cands\n";
+                    for (auto &sp : sb1cands) {
+                        int ind = sp.first;
+                        sb1inds.push_back(ind);
+                        cout << "\tEdge " << ind << "\n\t\ttour: "
+                             << tour_edges[ind] << "\t"
+                             << "lp: " << x[ind] << "\t"
+                             << "Est: " << sp.second << "\n";
                     }
-
-                    std::sort(sb1_scores.begin(), sb1_scores.end(),
-                              sp_sort_greater);
-
-                    for (ScorePair &p : sb1_scores)
-                        cout << "Var " << p.first << " has sb1 score "
-                             << p.second << "\n";
-
-                    vector<int> sb1_5_cands{sb1_scores[0].first,
-                                            sb1_scores[1].first};
-
-                    double sb2 = CMR::util::zeit();
-
-                    cout << "500 itlim primal sb w 2 cands...";
-                    REQUIRE_NOTHROW(rel.primal_strong_branch(tour_edges,
-                                                             tour_colstat,
-                                                             tour_rowstat,
-                                                             sb1_5_cands,
-                                                             downobj,
-                                                             upobj, 500,
-                                                             tourval));
-                    cout << "Done in " << (CMR::util::zeit() - sb2) << "s\n";
-
-                    vector<ScorePair> sb2_scores;
-                    
-                    for (int i = 0; i < 2; ++i) {
-                        ScorePair p(sb1_5_cands[i],
-                                  CMR::LP::branch_score(100, downobj[i],
-                                                        upobj[i]));
-                        sb2_scores.push_back(p);
-                    }
-
-                    for (ScorePair &p : sb2_scores)
-                        cout << "Var " << p.first << " has sb2 score "
-                             << p.second << "\n";
-
-                    cout << "Doing CPLEX dual sb 100 itlim 5 cands NO opt....";
-                    double dsb = CMR::util::zeit();
-
-                    REQUIRE_NOTHROW(rel.dual_strong_branch(cand_inds,
-                                                           downobj, upobj,
-                                                           100, tourval));
-                    cout << "Done in " << (CMR::util::zeit() - dsb) << "s\n";
-
-                    vector<ScorePair> dsb_scores;
-
-                    for (int i = 0; i < cand_inds.size(); ++i) {
-                        ScorePair p(cand_inds[i],
-                                    CMR::LP::branch_score(100, downobj[i],
-                                                          upobj[i]));
-                        dsb_scores.push_back(p);
-                    }
-
-                    cout << "\tRaw dual sb data\n";
-                    for (int i = 0; i < cand_inds.size(); ++i) {
-                        cout << "var " << cand_inds[i] << " downobj: "
-                             << downobj[i] << ", upobj: "
-                             << upobj[i] << "tour vec: "
-                             << tour_edges[i] << "\n";
-                    }
-
-                    std::sort(dsb_scores.begin(), dsb_scores.end(),
-                              sp_sort_greater);
-
-                    for (ScorePair &p : dsb_scores)
-                        cout << "Var " << p.first << " has sb2 score "
-                             << p.second << "\n";
+                    cout << "\n";                    
                 }
             }
         }
