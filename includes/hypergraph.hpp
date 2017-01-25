c94a0862252ca964eec32449b40a56ee161bf6ec
christos2 <c.stratopoulos@gmail.com>
2017-01-25 10:32:39 -0500

    made get_duals a template method

--- includes/hypergraph.hpp
+++ includes/hypergraph.hpp
@@ -5,8 +5,11 @@
 #include "lp_interface.hpp"
 #include "cliq.hpp"
 #include "price_util.hpp"
+#include "err_util.hpp"
 
+#include <iostream>
 #include <map>
+#include <stdexcept>
 #include <utility>
 #include <vector>
 
@@ -107,11 +110,12 @@ public:
                  std::vector<int> &cmatind, std::vector<double> &cmatval);
 
     /// Retrieve exact/estimated duals for use in computing reduced costs.
+    template <typename numtype>
     void get_duals(const LP::Relaxation &relax,
-                   std::vector<double> &node_pi,
-                   std::vector<double> &node_pi_est,
-                   std::vector<double> &cut_pi,
-                   std::unordered_map<Clique, double> &clique_pi) const;
+                   std::vector<numtype> &node_pi,
+                   std::vector<numtype> &node_pi_est,
+                   std::vector<numtype> &cut_pi,
+                   std::unordered_map<Clique, numtype> &clique_pi) const;
 
 private:
     /// Number of nodes in the Instance being tracked.
@@ -123,6 +127,136 @@ private:
 
     std::vector<HyperGraph> cuts; //<! List of the cuts in the LP::Relaxation.
 };
+
+/**
+ * This method will use the LP::Relaxation to query the lp solver for dual 
+ * values, and use the ExternalCuts collection of cuts, cliques, and teeth to
+ * generate clique multiplicities and node pi estimates for use in exactly
+ * pricing edges not currently in the Relaxation.
+ * @param[in] relax the core lp relaxation, for use in grabbing node and cut
+ * pi values from the lp solver.
+ * @param[in,out] node_pi a vector for storing dual values associated to 
+ * degree constraints
+ * @param[in,out] node_pi_est a vector for storing estimated degree constraint
+ * duals, computed Standard HyperGraph duals and by overestimating the 
+ * Domino HyperGraph duals.
+ * @param[in,out] cut_pi the dual values associated to cuts in the Relaxation.
+ * @param[in,out] clique_pi a hash table of multiplicities associated to 
+ * Cliques from the source CliqueBank.
+ */
+template <typename numtype>
+void ExternalCuts::get_duals(const LP::Relaxation &relax,
+                             std::vector<numtype> &node_pi,
+                             std::vector<numtype> &node_pi_est,
+                             std::vector<numtype> &cut_pi,
+                             std::unordered_map<Clique,
+                             numtype> &clique_pi) const
+{
+    using CutType = HyperGraph::Type;
+    using std::vector;
+    
+    std::runtime_error err("Problem in ExternalCuts::get_duals.");
+
+    vector<double> d_node_pi;
+    vector<double> d_cut_pi;
+
+
+    if (relax.num_rows() != node_count + cuts.size()) {
+        std::cerr << "Relaxation row count: " << relax.num_rows() << ", "
+                  << "ExternalCuts expects: "
+                  << (node_count + cuts.size()) << "\n";
+        throw std::logic_error("Size mismatch in ExternalCuts::get_duals.");
+    }
+
+    clique_pi.clear();
+
+    try {
+        clique_pi.reserve(clique_bank.size());
+
+        relax.get_pi(d_node_pi, 0, node_count - 1);
+
+
+        if (!cuts.empty())
+            relax.get_pi(d_cut_pi, node_count, relax.num_rows() - 1);
+    } CMR_CATCH_PRINT_THROW("getting/allocating double pi containers.", err);
+
+    try {
+        node_pi = vector<numtype>(d_node_pi.begin(), d_node_pi.end());
+        cut_pi = vector<numtype>(d_cut_pi.begin(), d_cut_pi.end());
+        node_pi_est = node_pi;
+    } CMR_CATCH_PRINT_THROW("copying to result vectors", err);
+
+    //get clique_pi for non-domino cuts
+    for (int i = 0; i < cuts.size(); ++i) {
+        const Sep::HyperGraph &H = cuts[i];
+        if (H.cut_type() == CutType::Domino)
+            continue;
+
+        numtype pival = cut_pi[i];
+
+        for (const Sep::Clique::Ptr &clq_ref : H.get_cliques()) {
+            if (clique_pi.count(*clq_ref) == 0)
+                clique_pi[*clq_ref] = 0.0;
+            
+            clique_pi[*clq_ref] += pival;
+        }
+    }
+
+    const vector<int> &def_tour = clique_bank.ref_tour();
+
+    //use clique_pi to build node_pi for all cliques with nonzero pi
+    for (const std::pair<Sep::Clique, numtype> &kv : clique_pi) {
+        const Sep::Clique &clq = kv.first;
+        numtype pival = kv.second;
+
+        if (pival > 0.0) {
+            for (const Segment &seg : clq.seg_list()) {
+                for (int k = seg.start; k <= seg.end; ++k) {
+                    int node = def_tour[k];
+                    
+                    node_pi[node] += pival;
+                    node_pi_est[node] += pival;
+                }
+            }            
+        } else if (pival < 0.0) {
+            for (const Segment &seg : clq.seg_list()) {
+                for (int k = seg.start; k <= seg.end; ++k) {
+                    int node = def_tour[k];
+                    
+                    node_pi[node] += pival;
+                }
+            }
+        }
+    }
+
+    //now get node_pi_est for domino cuts, skipping standard ones
+    for (int i = 0; i < cuts.size(); ++i) {
+        const Sep::HyperGraph &H = cuts[i];
+        if (H.cut_type() != CutType::Domino)
+            continue;
+
+        numtype pival = cut_pi[i];
+        if (pival <= 0.0)
+            continue;
+        
+        const Sep::Clique::Ptr &handle_ref = H.get_cliques()[0];
+        for (const Segment &seg : handle_ref->seg_list()) {
+            for (int k = seg.start; k <= seg.end; ++k) {
+                int node = def_tour[k];
+                node_pi_est[node] += pival;
+            }
+        }
+
+        for (const Sep::Tooth::Ptr &T : H.get_teeth())
+            for (const Sep::Clique &tpart : T->set_pair())
+                for (const Segment &seg : tpart.seg_list())
+                    for (int k = seg.start; k <= seg.end; ++k) {
+                        int node = def_tour[k];
+
+                        node_pi_est[node] += pival;
+                    }
+    }    
+}
 
 }
 }
