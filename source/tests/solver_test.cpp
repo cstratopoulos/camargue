d63a65eb32ab4096d58223e37bd975f9568b6baa
christos2 <c.stratopoulos@gmail.com>
2017-02-10 20:43:03 -0500

    rename

--- source/tests/solver_test.cpp
+++ source/tests/solver_test.cpp
@@ -26,6 +26,27 @@ using std::string;
 using std::to_string;
 using std::cout;
 
+SCENARIO ("Trying Solver cheat mode",
+          "[Solver][Cheat][cc_elim_mode]") {
+    using namespace CMR;
+    vector<string> probs{
+        "d493",
+        "pr1002",
+        "d2103",
+        };
+
+    for (string &prob : probs) {
+        GIVEN ("A Solver for " + prob) {
+            OutPrefs prefs;
+            int seed = 99;
+            Solver solver("problems/" + prob + ".tsp", seed, prefs);
+            THEN ("We can set up cheat mode") {
+                REQUIRE_NOTHROW(solver.cc_elim_mode());
+            }
+        }
+    }
+}
+
 
 
 SCENARIO ("Running the Solver cutting_loop with no pricing.",
