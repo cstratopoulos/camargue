2c599aed2d0438f3c035460b1f675d645918a1b3
christos2 <c.stratopoulos@gmail.com>
2017-02-18 11:19:08 -0500

    more smaller examples

--- source/tests/cutpool_test.cpp
+++ source/tests/cutpool_test.cpp
@@ -27,12 +27,15 @@ SCENARIO ("Pricing cuts from a cutpool",
     using namespace CMR;
     using ProbPair = std::pair<string, int>;
 
-    //trying to record seeds with lots of augmentations
+    //trying to record seeds with lots of (or at least some) augmentations
     vector<ProbPair> probs{
         ProbPair("d493", 99),
+        ProbPair("gr666", 1487433073),
+        ProbPair("u724", 1487432520),
         ProbPair("pr1002", 1487347814),
         ProbPair("d2103", 1487347819),
-        ProbPair("pcb3038", 0),
+        ProbPair("pr2392", 1487353952),
+        ProbPair("pcb3038", 99),
         };
 
     for (ProbPair &pp : probs) {
@@ -42,7 +45,10 @@ SCENARIO ("Pricing cuts from a cutpool",
             THEN ("We can verify the cut prices if the cut pool is nonempty") {
                 OutPrefs prefs;
                 Solver solver("problems/" + prob + ".tsp", seed, prefs);
+
+                solver.cut_sel.cutpool = false;
                 solver.cutting_loop(false, true, true);
+
                 Graph::CoreGraph &G =
                 const_cast<Graph::CoreGraph &>(solver.graph_info());
 
@@ -60,6 +66,9 @@ SCENARIO ("Pricing cuts from a cutpool",
                 cout << "\tPool has " << pool.size() << " cuts\n";
 
                 vector<double> lp_vec = core.lp_vec();
+
+                REQUIRE(lp_vec != solver.tour_basis().best_tour_edges);
+
                 const vector<Graph::Edge> &edges = G.get_edges();
                 vector<int> island;
 
@@ -69,11 +78,15 @@ SCENARIO ("Pricing cuts from a cutpool",
                 bool found_pool = false;
                 double pt = util::zeit();
                 REQUIRE_NOTHROW(found_pool = pool_sep.price_cuts());
-                cout << "\tPriced pool in " << (util::zeit() - pt) << "s\n";
-                if (!found_pool)
-                    continue;
+                pt = util::zeit() - pt;
+                cout << "\tPriced pool in " << pt << "s\n";
 
-                cout << "\tViolated cuts in pool.\n\n";
+                if (found_pool)
+                    cout << "\tViolated cuts in pool.\n\n";
+                else {
+                    cout << "\tNo violated cuts in pool.\n\n";
+                }
+
                 bool found_viol = false;
                 const vector<double> &slacks = pool_sep.get_slacks();
                 for (int i = 0; i < slacks.size(); ++i) {
@@ -86,17 +99,22 @@ SCENARIO ("Pricing cuts from a cutpool",
 
                     double activity = Sep::get_activity(s_dat.lp_vec, R);
                     double manual_slack;
-                    if (H.get_sense() == 'L')
+                    string sense_str = "\0";
+                    if (H.get_sense() == 'L') {
                         manual_slack = rhs - activity;
-                    else if (H.get_sense() == 'G')
+                        sense_str = " <= ";
+                    } else if (H.get_sense() == 'G') {
+                        sense_str = " >= ";
                         manual_slack = activity - rhs;
+                    }
 
-                    INFO ("Cut type " << H.cut_type());
+                    INFO("Cut type " << H.cut_type());
+                    INFO("Cut: " << activity << sense_str << rhs) ;
                     REQUIRE(manual_slack == Approx(pool_slack));
-                    if (manual_slack <= Epsilon::Cut)
+                    if (manual_slack <= -Epsilon::Cut)
                         found_viol = true;
                 }
-                REQUIRE(found_viol);
+                REQUIRE(found_viol == found_pool);
             }
         }
     }
