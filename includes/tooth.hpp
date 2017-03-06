fd15227f592279a2e98c32eb06a2bd69bf58644f
christos2 <c.stratopoulos@gmail.com>
2017-03-06 11:38:27 -0500

    new getrange version

--- includes/tooth.hpp
+++ includes/tooth.hpp
@@ -27,15 +27,17 @@ public:
 
     void get_light_teeth();
 
-    static IntPair get_range(const int root, const ToothBody &s,
-                             const std::vector<std::vector<int>> &zones);
+    /// Get the range of adjacency zones for a tooth body wrt a given root.
+    static IntPair get_range(ToothBody s,
+                             const std::vector<int> &perm,
+                             const std::vector<Graph::AdjObj> &root_nbrs);
 
-    static bool root_equivalent(const int root, const ToothBody &s1,
-                                const ToothBody &s2,
-                                const std::vector<std::vector<int>> &zones);
+    static bool root_equivalent(int root, ToothBody s1, ToothBody s2,
+                                const std::vector<int> &tour,
+                                const std::vector<int> &perm,
+                                const std::vector<Graph::Node> &nodelist);
 
-    bool root_equivalent(const int root, const ToothBody &s1,
-                         const ToothBody &s2) const;
+    bool root_equivalent(int root, ToothBody s1, ToothBody s2) const;
 
     void sort_by_root();
 
@@ -51,7 +53,6 @@ public:
 
     std::vector<ToothList> light_teeth;
 
-    static std::vector<std::vector<int>> adj_zones;
     static std::vector<IteratorMat> seen_ranges;
     std::vector<std::array<int, 3>> list_sizes;
     std::vector<ListStat> stats;
@@ -62,27 +63,26 @@ private:
     std::vector<int> endmark;
 
     static void add_tooth(ToothList &teeth,
-                          const std::vector<std::vector<int>> &zones,
                           std::vector<IteratorMat> &ranges,
                           std::array<int, 3> &sizes,
-                          const int root, const int body_start,
-                          const int body_end, const double slack);
+                          int root, int body_start,
+                          int body_end, double slack,
+                          const std::vector<int> &tour,
+                          const std::vector<int> &perm,
+                          const std::vector<Graph::Node> &nodelist);
 
     static int teeth_cb(double cut_val, int cut_start, int cut_end,
                         void *u_data);
 
     struct LinsubCBData {
         LinsubCBData(std::vector<ToothList> &_light_teeth,
-                     std::vector<std::vector<int>> &_adj_zones,
-                     std::vector<IteratorMat>
-                     &_ranges,
+                     std::vector<IteratorMat> &_ranges,
                      std::vector<std::array<int, 3>> &_list_sizes,
                      std::vector<bool> &_node_marks,
                      const std::vector<int> &_tour_nodes,
                      const std::vector<int> &_perm,
                      const Graph::AdjList &_G_s) :
-            light_teeth(_light_teeth),
-            adj_zones(_adj_zones), ranges(_ranges), list_sizes(_list_sizes),
+            light_teeth(_light_teeth),ranges(_ranges), list_sizes(_list_sizes),
             node_marks(_node_marks),
             tour_nodes(_tour_nodes), perm(_perm),
             G_s(_G_s),
@@ -90,7 +90,6 @@ private:
             {}
 
         std::vector<std::vector<SimpleTooth::Ptr>> &light_teeth;
-        std::vector<std::vector<int>> &adj_zones;
         std::vector<IteratorMat> &ranges;
         std::vector<std::array<int, 3>> &list_sizes;
 
