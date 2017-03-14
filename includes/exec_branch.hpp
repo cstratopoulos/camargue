67cbe0ed8a93a5c3b7147a06582fe09c1c20c797
christos2 <c.stratopoulos@gmail.com>
2017-03-14 11:19:33 -0400

    BranchNode stuff in its own file

--- includes/exec_branch.hpp
+++ includes/exec_branch.hpp
@@ -11,6 +11,7 @@
 #include "core_lp.hpp"
 #include "datagroups.hpp"
 #include "cliq.hpp"
+#include "branch_node.hpp"
 #include "lp_util.hpp"
 #include "branch_util.hpp"
 
@@ -23,69 +24,6 @@
 
 namespace CMR {
 namespace ABC {
-
-struct BranchNode {
-    enum Dir : int {
-        Down = 0,
-        Up = 1
-    };
-
-    enum class Status {
-        NeedsCut,
-        NeedsBranch,
-        NeedsPrice,
-        NeedsRecover,
-        Pruned,
-        Done,
-    };
-
-    BranchNode(); //!< Construct a root node.
-
-    /// Construct a child node.
-    BranchNode(EndPts ends_, Dir direction_, const BranchNode &parent_,
-               double tourlen_, double estimate_);
-
-    BranchNode(BranchNode &&B) noexcept;
-    BranchNode &operator=(BranchNode &&B) noexcept;
-
-    /// Alias declaration for returning two split child problems.
-    using Split = std::array<BranchNode, 2>;
-
-    EndPts ends; //!< The endpoints of the branch edge.
-    Dir direction; //!< Down branch or up branch.
-
-    Status stat; //!< The type of processing required by the node.
-
-    const BranchNode *parent;
-    int depth; //!< Search tree depth of this node.
-
-    double tourlen; //!< Estimated best tour length for this node.
-
-    /// A starting basis for if Status is NeedsPrice or NeedsRecover.
-    LP::Basis::Ptr price_basis;
-    double estimate; //!< The objective value estimate from edge selection.
-
-    /// Is this the root problem.
-    bool is_root() const { return parent == nullptr; }
-
-    /// Has the problem been processed.
-    bool visited() const
-        { return stat == Status::Pruned || stat == Status::Done; }
-};
-
-/// Turn a 0-1 value \p entry into a BranchNode::Dir.
-BranchNode::Dir dir_from_int(int entry);
-
-/// A concise "label" for the BranchNode \p B.
-std::string bnode_brief(const BranchNode &B);
-
-std::ostream &operator<<(std::ostream &os, const BranchNode::Dir &dir);
-std::ostream &operator<<(std::ostream &os, const BranchNode &B);
-
-using BranchHistory = std::list<BranchNode>;
-
-/// Alias declaration for EndPts and branching direction.
-using EndsDir = std::pair<EndPts, BranchNode::Dir>;
 
 class Executor {
 public:
--- source/exec_branch.cpp
+++ source/exec_branch.cpp
@@ -3,7 +3,6 @@
 
 #include <algorithm>
 #include <iostream>
-#include <sstream>
 #include <stdexcept>
 
 extern "C" {
@@ -16,123 +15,17 @@ using std::vector;
 using std::cout;
 using std::cerr;
 using std::endl;
-using std::ostream;
 
 using std::runtime_error;
 using std::logic_error;
 using std::exception;
 
 namespace CMR {
+namespace ABC {
 
 constexpr int IntMax = std::numeric_limits<int>::max();
 constexpr double DoubleMax = std::numeric_limits<double>::max();
 
-namespace ABC {
-
-BranchNode::BranchNode() : stat(Status::NeedsCut),
-                           parent(nullptr), depth(0),
-                           tourlen(IntMax),
-                           estimate(DoubleMax) {}
-
-BranchNode::BranchNode(EndPts ends_, Dir direction_,
-                       const BranchNode &parent_,
-                       double tourlen_, double estimate_)
-    : ends(ends_), direction(direction_), stat(Status::NeedsCut),
-      parent(&parent_), depth(1 + parent_.depth),
-      tourlen(tourlen_), estimate(estimate_) {}
-
-BranchNode::BranchNode(BranchNode &&B) noexcept
-    : ends(std::move(B.ends)),
-      direction(B.direction),
-      stat(B.stat),
-      parent(B.parent),
-      depth(B.depth),
-      tourlen(B.tourlen),
-      price_basis(std::move(B.price_basis)),
-      estimate(B.estimate)
-{
-    B.stat = Status::Done;
-
-    B.parent = nullptr;
-    B.depth = 0;
-
-    B.tourlen = IntMax;
-    B.estimate = DoubleMax;
-}
-
-BranchNode &BranchNode::operator=(BranchNode &&B) noexcept
-{
-    if (&B == this)
-        return *this;
-
-    ends = std::move(B.ends);
-    direction = B.direction;
-
-    stat = B.stat;
-
-
-    parent = B.parent;
-    depth = B.depth;
-
-    tourlen = B.tourlen;
-
-    estimate = B.estimate;
-    price_basis = std::move(B.price_basis);
-
-    B.stat = Status::Done;
-
-    B.parent = nullptr;
-    B.depth = 0;
-
-    B.tourlen = IntMax;
-    B.estimate = DoubleMax;
-
-    return *this;
-}
-
-BranchNode::Dir dir_from_int(int entry)
-{
-    switch (entry) {
-    case 0:
-        return BranchNode::Dir::Down;
-    case 1:
-        return BranchNode::Dir::Up;
-    default:
-        throw runtime_error("Tried to get BranchNode::Dir w non-binary int");
-    }
-}
-
-std::string bnode_brief(const BranchNode &B)
-{
-    std::stringstream result;
-
-    if (B.is_root())
-        result << "[root]";
-    else {
-        result << "[" << B.ends << " = " << B.direction << "]";
-    }
-
-    return result.str();
-}
-
-ostream &operator<<(ostream &os, const BranchNode::Dir &dir)
-{
-    os << static_cast<int>(dir);
-    return os;
-}
-ostream &operator<<(ostream &os, const BranchNode &B)
-{
-    os << bnode_brief(B) << ", ";
-
-    if (B.is_root()) {
-        os << "depth 0";
-    } else {
-        os << "depth " << B.depth << ", tourlen " << B.tourlen
-           << ", parent "
-           << bnode_brief(*(B.parent));
-    }
-    return os;
-}
 
 Executor::Executor(const Data::Instance &inst,
                    const LP::ActiveTour &activetour,
