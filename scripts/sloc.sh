626c229e1b42a964df18eb31dfd843ea4f787ceb
christos2 <c.stratopoulos@gmail.com>
2017-03-31 16:33:32 -0400

    removed unused script

--- scripts/sloc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cat $* | # cat input files into pipe
-sed '/^\ *\/\// d' | #delete // style comments
-sed '/^\ *[ \/\\]\*/ d' | #delete /* style comments and \* and /* ' *'
-#sed '/^#include/ d' | #delete include statements
-wc -l
